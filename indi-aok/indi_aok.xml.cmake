a75c3b11f69657747d9702e46683c938eece1cbc
escribana <escriban@bluewin.ch>
2019-10-13 10:39:11 +0200

    INDIdriver for the new direct drive mounts 'AOK Skywalker' from AO… (#1)
    * INDIdriver for the new direct drive mount "Herkules V24" from AOKSwiss/
    Magnetdrives.
    
    * Delete CMakeLists.txt.user
    
    Not required.
    
    * The specialties of the TCS (Telescope Control System) of the Herkules
    mount are now implemented. (Track lock and Drives lock)
    
    The workflow of ReadScopeStatus() adapted to special TCS-commands
    
    * Driver is renamed to reflect the support for the whole product line of
    the AOK Skywalker mounts (e.g. all Herkules mounts an the Atlas mount).
    
    The logic of the starting sequence is enhanced (Unpark is done automaticly).
    
    * Debain files uploaded
    
    * Delete CMakeLists.txt.user
    
    Sorry!

---
+++ indi-aok/indi_aok.xml.cmake
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="Telescopes">
+    <device label="AOK Skywalker" manufacturer="AOKSwiss/Magnetdrives">
+        <driver name="LX200 AOK">indi_lx200aok</driver>
+        <version>@SKYWALKER_VERSION_MAJOR@.@SKYWALKER_VERSION_MINOR@</version>
+    </device>
+</devGroup>
+</driversList>
