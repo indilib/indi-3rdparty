f506639a2907cde3f72031ab9ad109bb4dff5c5c
Jasem Mutlaq <mutlaqja@ikarustech.com>
2019-10-21 21:09:14 +0300

    Add Sony to stop live preview as well.
    Fixes #1034

--- indi-gphoto/gphoto_driver.cpp
+++ indi-gphoto/gphoto_driver.cpp
@@ -2126,8 +2126,8 @@ int gphoto_start_preview(gphoto_driver *gphoto)
 
 int gphoto_stop_preview(gphoto_driver *gphoto)
 {
-    // Olympus cameras support streaming but without viewfinder_widget
-    if (strcasestr(gphoto->manufacturer, "OLYMPUS"))
+    // Olympus & Sony cameras support streaming but without viewfinder_widget
+    if (strcasestr(gphoto->manufacturer, "OLYMPUS") || strcasestr(gphoto->manufacturer, "Sony Corporation"))
         return GP_OK;
 
     // If viewfinder not found, nothing to do
