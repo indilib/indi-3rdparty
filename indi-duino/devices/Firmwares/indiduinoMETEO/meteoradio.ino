36cdc5f464e3fbf487c48bb5290efa334f9a9a65
Wolfgang Reissenberger <sterne-jaeger@t-online.de>
2019-12-25 20:14:31 +0100

    Initial version of meteoradio

---
+++ indi-duino/devices/Firmwares/indiduinoMETEO/meteoradio.ino
@@ -0,0 +1,75 @@
+
+/*  Firmware for a weather sensor device streaming the data as JSON documents.
+
+    Copyright (C) 2019 Wolfgang Reissenberger <sterne-jaeger@t-online.de>
+
+    This application is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+*/
+#include <ArduinoJson.h>
+// #include <MemoryFree.h> // only necessary for debugging
+
+// uncomment to write sensor data to console
+#define DEBUG
+
+#define USE_BME_SENSOR            //USE BME280 ENVIRONMENT SENSOR. Comment if not.
+
+#ifdef USE_BME_SENSOR
+#include "Adafruit_BME280.h"
+Adafruit_BME280 bme;
+struct {
+  bool status;
+  float temperature;
+  float pressure;
+  float humidity;
+} bmeData;
+
+
+JsonDocument serializeBME() {
+  StaticJsonDocument<JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3)> bmeJO;
+
+  JsonObject data = bmeJO.createNestedObject("BME280");
+  data["T"] = bmeData.temperature;
+  data["P"] = bmeData.pressure;
+  data["H"] = bmeData.humidity;
+
+  return bmeJO;
+}
+
+void updateBME() {
+  if (bmeData.status || (bmeData.status = bme.begin())) {
+    bmeData.temperature = bme.readTemperature();
+    bmeData.pressure    = bme.readPressure() / 100.0;
+    bmeData.humidity    = bme.readHumidity();
+  }
+  else {
+    bmeData.status = false;
+    Serial.println("BME sensor initialization FAILED!");
+  }
+
+#ifdef DEBUG
+  serializeJson(serializeBME(), Serial);
+  Serial.println();
+#endif //DEBUG  
+}
+#endif //USE_BME_SENSOR
+
+
+void setup() {
+  Serial.begin(9600);
+  // wait for serial port to connect. Needed for native USB
+  while (!Serial) continue;
+
+}
+
+void loop() {
+
+#ifdef USE_BME_SENSOR
+  updateBME();
+#endif //USE_BME_SENSOR
+
+  delay(500);
+  //Serial.print("free Memory="); Serial.println(freeMemory());
+}
