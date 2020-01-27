7ac6e4d489f82e1a1dd0a1132daa8a8f09a8d6c5
karlrees <github@karlrees.com>
2020-01-27 06:10:54 +0100

    Updated how the libraries were being installed / linked to avoid conflicts between the standard version of libmtp and the custom version of libmtp used by the Ricoh SDK.

--- indi-pentax/config.h
+++ indi-pentax/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 /* Define INDI Data Dir */
-#define INDI_DATA_DIR "/usr/share/indi"
+#define INDI_DATA_DIR "/usr/local/share/indi"
 /* Define Driver version */
 #define INDI_PENTAX_VERSION_MAJOR 1
 #define INDI_PENTAX_VERSION_MINOR 0
