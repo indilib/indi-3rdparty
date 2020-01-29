843f66705e58324371dc36652339c1ad19f65787
Wolfgang Reissenberger <sterne-jaeger@t-online.de>
2020-01-29 21:00:48 +0100

    First line contains current weather values

--- indi-duino/add-on/weatherradio/html/js/charts.js
+++ indi-duino/add-on/weatherradio/html/js/charts.js
@@ -1,6 +1,5 @@
 function createWeatherChart(category, align) {
 
-    var offsetX = (align == 'left') ? 30 : 0;
     var chart = {
 	height: 250,
 	type: "area",
@@ -12,16 +11,9 @@ function createWeatherChart(category, align) {
 	}
     };
     var title = {
-	text: "--",
-	align: align,
-	offsetX: offsetX,
-	style: {fontSize: '24px', color: '#ccc'}
-    };
-    var subtitle = {
 	text: category,
 	align: align,
-	offsetX: offsetX,
-	style: {fontSize: '16px', color: '#ccc'}
+	style: {fontSize: '14px', color: '#ccc'}
     };
     var xaxis = {
 	type: "datetime",
@@ -33,28 +25,124 @@ function createWeatherChart(category, align) {
 
     return {chart: chart,
 	    title: title,
-	    subtitle: subtitle,
 	    xaxis: xaxis,
 	    yaxis: yaxis,
 	    series: [],
-	    stroke: {curve: 'smooth'},
+	    stroke: {curve: 'smooth', width: 1},
 	    tooltip: {x: {format: "dd MMM yy, HH:mm"}},
 	    dataLabels: {enabled: false}};
 }
 
+function createRadialBarChart(name, unit, min, max) {
+    return ({
+	chart: {
+	    height: 300,
+	    type: 'radialBar',
+	},
+	// set to 0
+	series: [100 * (0 - min) / (max - min)],
+	labels: [name],
+	stroke: {
+	    dashArray: 3
+	},
+	plotOptions: {
+	    radialBar: {
+		startAngle: -135,
+		endAngle: 135,
+		dataLabels: {
+		    name: {show: true},
+		    value: {
+			show: true,
+			fontSize: "30px",
+			color: "#ccc",
+			formatter: function(val) {
+			    return parseInt(min + (max - min) * val/100) + unit;
+			},
+		    }
+		},
+		track: {show: true, opacity: 0.1},
+	    }
+	},
+    });
+
+}
+
+function createBarChart(name, unit, min, max) {
+    return ({
+        series: [
+	    {
+		name: name,
+		data: [50]
+	    }],
+        chart: {
+	    type: 'bar',
+	    height: 80,
+	    width: "50%",
+	    toolbar: {show: false},
+	},
+	offsetX: 0,
+	offsetY: 0,
+        plotOptions: {bar: {horizontal: true}},
+        dataLabels: {
+	    enabled: true,
+	    style: {colors: ["#ccc"], fontSize: "14px"},
+	    formatter: function(val) {
+		return parseInt(val) + '%';
+	    },
+	},
+	title: {
+	    text: name,
+	    align: "center",
+	    offsetY: 0,
+	    floating: true,
+	    style: {color: '#ccc', fontSize: "14px"}
+	},
+        xaxis: {
+	    show: false,
+	    categories: ['Humidity'],
+	    labels: {show: false},
+	    axisTicks: {show: false},
+	},
+	yaxis: {labels: {show: false}, min: min, max: max},
+	tooltip: {enabled: false}
+    });
+
+    
+}
+
 var hchart, cchart, tchart, pchart;
+var temperature, humidity, pressure, cloudCoverage;
+
+var settings = {t_min: -40, t_max: 50, p_min: 940, p_max: 990};
 
 function init() {
 
-    // create the charts
-    tchart = new ApexCharts(document.querySelector("#temperature"),
-			    createWeatherChart("Temperature", "left"));
-    cchart = new ApexCharts(document.querySelector("#clouds"),
-			    createWeatherChart("Cloud Coverage", "right"));
-    pchart = new ApexCharts(document.querySelector("#pressure"),
-			    createWeatherChart("Pressure", "left"));
-    hchart = new ApexCharts(document.querySelector("#humidity"),
-			    createWeatherChart("Humidity", "right"));
+    // create charts for current values
+    temperature = new ApexCharts(document.querySelector("#temperature"),
+				 createRadialBarChart('Temperature', '°C',
+						      settings.t_min, settings.t_max));
+    humidity = new ApexCharts(document.querySelector("#humidity"),
+			      createBarChart('Humidity', '%', 0, 100));
+    pressure = new ApexCharts(document.querySelector("#pressure"),
+			      createRadialBarChart('Pressure', ' hPa',
+						   settings.p_min, settings.p_max));
+    cloudCoverage = new ApexCharts(document.querySelector("#clouds"),
+				   createBarChart('Cloud Coverage', '%', 0, 100));
+    temperature.render();
+    humidity.render();
+    pressure.render();
+    cloudCoverage.render();
+    
+    // create the time series charts
+    
+    tchart = new ApexCharts(document.querySelector("#temperature_series"),
+			    createWeatherChart("Temperature", "center"));
+    hchart = new ApexCharts(document.querySelector("#humidity_series"),
+			    createWeatherChart("Humidity", "center"));
+    pchart = new ApexCharts(document.querySelector("#pressure_series"),
+			    createWeatherChart("Pressure", "center"));
+    cchart = new ApexCharts(document.querySelector("#clouds_series"),
+			    createWeatherChart("Cloud Coverage", "center"));
 
     hchart.render();
     cchart.render();
@@ -73,11 +161,17 @@ function updateSeries() {
 	tchart.updateSeries([data.T]);
 	pchart.updateSeries([data.P]);
 
-	// update last value
-	tchart.updateOptions({title: {text: (data.T.data[data.T.data.length-1][1]).toFixed(0) + "°C"}});
-	cchart.updateOptions({title: {text: (data.clouds.data[data.clouds.data.length-1][1]).toFixed(0) + "%"}});
-	pchart.updateOptions({title: {text: (data.P.data[data.P.data.length-1][1]).toFixed(0) + " hPa"}});
-	hchart.updateOptions({title: {text: (data.HR.data[data.HR.data.length-1][1]).toFixed(0) + "%"}});
+	// update current value
+	var currentTemperature   = Math.round(data.T.data[data.T.data.length-1][1]);
+	var currentCloudCoverage = Math.round(data.clouds.data[data.clouds.data.length-1][1]);
+	var currentHumidity      = Math.round(data.HR.data[data.HR.data.length-1][1]);
+	var currentPressure      = Math.round(data.P.data[data.P.data.length-1][1])
+
+	// calculate filling percentage from current temperature 
+	temperature.updateSeries([100 * (currentTemperature - settings.t_min) / (settings.t_max - settings.t_min)]);
+	humidity.updateSeries([{name: "Humidity", data: [currentHumidity]}]);
+	pressure.updateSeries([100 * (currentPressure - settings.p_min) / (settings.p_max - settings.p_min)]);
+	cloudCoverage.updateSeries([{name: "Cloud Coverage", data: [currentCloudCoverage]}]);
 
     });
 
