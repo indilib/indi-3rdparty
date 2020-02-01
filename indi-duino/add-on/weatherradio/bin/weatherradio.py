2fc69d3bb1161199717b59df94675255881c595c
Wolfgang Reissenberger <sterne-jaeger@t-online.de>
2020-02-02 00:08:46 +0100

    Update script added

---
+++ indi-duino/add-on/weatherradio/bin/weatherradio.py
@@ -0,0 +1,43 @@
+#-----------------------------------------------------------------------
+# Python library for weather radio.
+#
+# Copyright (C) 2019 Wolfgang Reissenberger <sterne-jaeger@t-online.de>
+#
+# This application is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# Based upon ideas from indiduinoMETEO (http://indiduino.wordpress.com).
+#-----------------------------------------------------------------------
+
+from wr_config import *
+
+def connect(indi):
+    #connect ones to configure the port
+    connection = indi.get_vector(INDIDEVICE, "CONNECTION")
+    if connection.get_element("CONNECT").get_active() == False:
+        # set the configured port
+        indi.set_and_send_text(INDIDEVICE,"DEVICE_PORT","PORT",INDIDEVICEPORT)
+
+        # connect driver
+        connection.set_by_elementname("CONNECT")
+        indi.send_vector(connection)
+        print "CONNECT INDI Server host:%s port:%s device:%s" % (INDISERVER,INDIPORT,INDIDEVICE)
+        time.sleep(5)
+
+def readWeather(indi):
+    weather     = indi.get_vector(INDIDEVICE,WEATHER)
+    temperature = weather.get_element(WEATHER_TEMPERATURE).get_float()
+    pressure    = weather.get_element(WEATHER_PRESSURE).get_float()
+    humidity    = weather.get_element(WEATHER_HUMIDITY).get_float()
+    cloudCover  = weather.get_element(WEATHER_CLOUD_COVER).get_float()
+    sqm         = weather.get_element(WEATHER_SQM).get_float()
+    dewpoint    = weather.get_element(WEATHER_DEWPOINT).get_float()
+    skyTemp     = weather.get_element(WEATHER_SKY_TEMPERATURE).get_float()
+  
+    return (("Temperature", temperature), ("Pressure", pressure),
+            ("Humidity", humidity), ("Dewpoint", dewpoint),
+            ("CloudCover", cloudCover), ("SkyTemperature", skyTemp),
+            ("SQM", sqm));
+
---
+++ indi-duino/add-on/weatherradio/bin/wr_rrd_update.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+#-----------------------------------------------------------------------
+# Script for updating the RRD time series.
+#
+# Copyright (C) 2020 Wolfgang Reissenberger <sterne-jaeger@t-online.de>
+#
+# This application is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# Based upon ideas from indiduinoMETEO (http://indiduino.wordpress.com).
+#-----------------------------------------------------------------------
+
+import sys
+from indiclient import *
+from weatherradio import *
+import rrdtool
+
+try:
+    print "Updating data from \"%s\"@%s:%s" % (INDIDEVICE,INDISERVER,INDIPORT)
+
+    # open connection to the INDI server
+    indi=indiclient(INDISERVER,int(INDIPORT))
+
+    # ensure that the INDI driver is connected to the device
+    connect(indi)
+    
+    now=time.localtime()
+    json_dict={"TIME":time.strftime("%c",now)}
+    data = readWeather(indi)
+
+    indi.quit()
+
+except:
+    print "Updating data from \"%s\"@%s:%s FAILED!" % (INDIDEVICE,INDISERVER,INDIPORT)
+    sys.exit()
+
+
+updateString="N"
+for d in data:
+	print d[0],d[1]
+	updateString=updateString+":"+str(d[1])
+	json_dict[d[0]]=int(d[1]*100)/100.
+
+ret = rrdtool.update(RRDFILE,updateString);
+if ret:    
+	print rrdtool.error() 
