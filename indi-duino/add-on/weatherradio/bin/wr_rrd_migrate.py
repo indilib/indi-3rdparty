ca2dba586e81eb94fa08097f1c3dbe89d0c436ee
Wolfgang Reissenberger <sterne-jaeger@t-online.de>
2020-02-02 14:18:08 +0100

    Pressure changed to sea level (not migrated)

--- indi-duino/add-on/weatherradio/bin/wr_rrd_migrate.py
+++ indi-duino/add-on/weatherradio/bin/wr_rrd_migrate.py
@@ -31,13 +31,12 @@ parser.add_argument("outfile", default="weather.rrd",
 
 args=parser.parse_args()
 
-migrate(args.infile, "60")
-
 # 1min raw values for 24 hours, 5 min for 7*24 hours, 1hour for 1 year,
 # 1day dor 10 years.
-ret = rrdtool.create(args.outfile, "-r", args.infile, "-r", "normalized.rrd", "--step", "60",
+ret = rrdtool.create(args.outfile, "-r", args.infile, "--step", "60",
 		     "DS:Temperature=T[1]:GAUGE:600:U:U",
-		     "DS:Pressure=P[2]:GAUGE:600:U:U",
+                     # pressure data ignored since we change to sea level
+		     "DS:Pressure:GAUGE:600:U:U",
 		     "DS:Humidity=HR[1]:GAUGE:600:U:U",
 		     "DS:CloudCover=clouds[1]:GAUGE:600:U:U",
 		     "DS:SkyTemperature=skyT[1]:GAUGE:600:U:U",
@@ -45,16 +44,16 @@ ret = rrdtool.create(args.outfile, "-r", args.infile, "-r", "normalized.rrd", "-
 		     "DS:SQM=Light[1]:GAUGE:600:U:U",
 		     "RRA:AVERAGE:0.5:1:1440",
 		     "RRA:AVERAGE:0.5:5:2016",
-		     "RRA:AVERAGE:0.5:3600:8760",
-		     "RRA:AVERAGE:0.5:86400:3650",
+		     "RRA:AVERAGE:0.5:60:8760",
+		     "RRA:AVERAGE:0.5:1440:3650",
 		     "RRA:MIN:0.5:1:1440",
 		     "RRA:MIN:0.5:5:2016",
-		     "RRA:MIN:0.5:3600:8760",
-		     "RRA:MIN:0.5:86400:3650",
+		     "RRA:MIN:0.5:60:8760",
+		     "RRA:MIN:0.5:1440:3650",
 		     "RRA:MAX:0.5:1:1440",
 		     "RRA:MAX:0.5:5:2016",
-                     "RRA:MAX:0.5:3600:8760",
-		     "RRA:MAX:0.5:86400:3650")
+                     "RRA:MAX:0.5:60:8760",
+		     "RRA:MAX:0.5:1440:3650")
 
 
 if ret:
