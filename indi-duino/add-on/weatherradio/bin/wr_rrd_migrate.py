35f3297fb470c63438534d2c4e78f68c3531575e
Wolfgang Reissenberger <sterne-jaeger@t-online.de>
2020-02-15 22:54:19 +0100

    Migration set back to meteostationWEB settings

--- indi-duino/add-on/weatherradio/bin/wr_rrd_migrate.py
+++ indi-duino/add-on/weatherradio/bin/wr_rrd_migrate.py
@@ -34,28 +34,27 @@ args=parser.parse_args()
 # 5min raw values for 24 hours, 15 min for 7*24 hours, 1hour for 1 year,
 # 1day dor 10 years.
 ret = rrdtool.create(args.outfile, "-r", args.infile, "--step", "300",
-		     "DS:Temperature=Temperature[1]:GAUGE:600:U:U",
+                     "DS:Temperature=T[1]:GAUGE:600:U:U",
                      # pressure data ignored since we change to sea level
-		     "DS:Pressure=Pressure[1]:GAUGE:600:U:U",
-		     "DS:Humidity=Humidity[1]:GAUGE:600:U:U",
-		     "DS:CloudCover=CloudCover[1]:GAUGE:600:U:U",
-		     "DS:SkyTemperature=SkyTemperature[1]:GAUGE:600:U:U",
-		     "DS:DewPoint=DewPoint[1]:GAUGE:600:U:U",
-		     "DS:SQM=SQM[1]:GAUGE:600:U:U",
-		     "RRA:AVERAGE:0.5:1:288",
-		     "RRA:AVERAGE:0.5:3:672",
-		     "RRA:AVERAGE:0.5:12:8760",
-		     "RRA:AVERAGE:0.5:288:3650",
-		     "RRA:MIN:0.5:1:288",
-		     "RRA:MIN:0.5:3:672",
-		     "RRA:MIN:0.5:12:8760",
-		     "RRA:MIN:0.5:288:3650",
-		     "RRA:MAX:0.5:1:288",
-		     "RRA:MAX:0.5:5:672",
+                     "DS:Humidity=HR[1]:GAUGE:600:U:U",
+                     "DS:CloudCover=clouds[1]:GAUGE:600:U:U",
+                     "DS:SkyTemperature=skyT[1]:GAUGE:600:U:U",
+                     "DS:DewPoint=Thr[1]:GAUGE:600:U:U",
+                     "DS:SQM=Light[1]:GAUGE:600:U:U",
+                     "RRA:AVERAGE:0.5:1:288",
+                     "RRA:AVERAGE:0.5:3:672",
+                     "RRA:AVERAGE:0.5:12:8760",
+                     "RRA:AVERAGE:0.5:288:3650",
+                     "RRA:MIN:0.5:1:288",
+                     "RRA:MIN:0.5:3:672",
+                     "RRA:MIN:0.5:12:8760",
+                     "RRA:MIN:0.5:288:3650",
+                     "RRA:MAX:0.5:1:288",
+                     "RRA:MAX:0.5:5:672",
                      "RRA:MAX:0.5:12:8760",
-		     "RRA:MAX:0.5:288:3650")
+                     "RRA:MAX:0.5:288:3650")
 
 
 if ret:
-		print rrdtool.error()
+    print rrdtool.error()
 
