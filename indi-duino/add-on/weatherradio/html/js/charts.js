2bc14aab56b8255c4ac592919dc169c925a45c76
Wolfgang Reissenberger <sterne-jaeger@t-online.de>
2020-02-15 18:20:14 +0100

    Untabified files

--- indi-duino/add-on/weatherradio/html/js/charts.js
+++ indi-duino/add-on/weatherradio/html/js/charts.js
@@ -1,94 +1,94 @@
 function createWeatherChart(category, unit, align, max, precision) {
 
     var chart = {
-	height: 250,
-	type: "area",
-	toolbar: {show: false},
+        height: 250,
+        type: "area",
+        toolbar: {show: false},
     };
     var title = {
-	text: category + " [" + unit + "]",
-	align: align,
-	offsetX: 6,
-	offsetY: 15,
-	style: {fontSize: '14px', color: '#ccc'}
+        text: category + " [" + unit + "]",
+        align: align,
+        offsetX: 6,
+        offsetY: 15,
+        style: {fontSize: '14px', color: '#ccc'}
     };
     var xaxis = {
-	type: "datetime",
-	labels: {style: {colors: '#ccc'}}
+        type: "datetime",
+        labels: {style: {colors: '#ccc'}}
     };
     var yaxis = {
-	labels: {style: {color: '#ccc'}},
-	decimalsInFloat: precision,
-	max: max
+        labels: {style: {color: '#ccc'}},
+        decimalsInFloat: precision,
+        max: max
     };
 
     return {chart: chart,
-	    subtitle: title,
-	    xaxis: xaxis,
-	    yaxis: yaxis,
-	    series: [],
-	    stroke: {curve: 'smooth', width: 2},
-	    legend: {labels: {colors: ['#ccc']}},
-	    tooltip: {x: {format: "dd MMM yy, HH:mm"},
-		      y: {formatter: function(value,
-					      {series, seriesIndex,
-					       dataPointIndex, w }) {
-			  return value.toFixed(precision) + unit;}}},
-	    dataLabels: {enabled: false}};
+            subtitle: title,
+            xaxis: xaxis,
+            yaxis: yaxis,
+            series: [],
+            stroke: {curve: 'smooth', width: 2},
+            legend: {labels: {colors: ['#ccc']}},
+            tooltip: {x: {format: "dd MMM yy, HH:mm"},
+                      y: {formatter: function(value,
+                                              {series, seriesIndex,
+                                               dataPointIndex, w }) {
+                          return value.toFixed(precision) + unit;}}},
+            dataLabels: {enabled: false}};
 }
 
 function createSparklineWeatherChart(category) {
 
     return {
-	chart: {
-	    height: 120,
-	    type: "area",
-	    sparkline: {enabled: true},
-	},
-	title: {
-	    text: category,
-	    align: 'center',
-	    offsetX: -10,
-	    offsetY: 10,
-	    style: {fontSize: '12px', color: '#ccc'}
-	},
-	series: [],
-	xaxis: {type: "datetime"},
-	stroke: {curve: 'smooth', width: 2},
-	tooltip: {x: {format: "dd MMM yy, HH:mm"}}};
+        chart: {
+            height: 120,
+            type: "area",
+            sparkline: {enabled: true},
+        },
+        title: {
+            text: category,
+            align: 'center',
+            offsetX: -10,
+            offsetY: 10,
+            style: {fontSize: '12px', color: '#ccc'}
+        },
+        series: [],
+        xaxis: {type: "datetime"},
+        stroke: {curve: 'smooth', width: 2},
+        tooltip: {x: {format: "dd MMM yy, HH:mm"}}};
 }
 
 function createRadialBarChart(name, unit, min, max, precision) {
     return ({
-	chart: {
-	    height: 300,
-	    type: 'radialBar',
-	},
-	// set to 0
-	series: [100 * (0 - min) / (max - min)],
-	labels: [name],
-	stroke: {
-	    dashArray: 3
-	},
-	plotOptions: {
-	    radialBar: {
-		startAngle: -135,
-		endAngle: 135,
-		dataLabels: {
-		    name: {show: true, offsetY: -15},
-		    value: {
-			show: true,
-			offsetY: 0,
-			fontSize: "30px",
-			color: "#ccc",
-			formatter: function(val) {
-			    return (min + (max - min) * val/100).toFixed(precision) + unit;
-			},
-		    }
-		},
-		track: {show: true, opacity: 0.1},
-	    }
-	},
+        chart: {
+            height: 300,
+            type: 'radialBar',
+        },
+        // set to 0
+        series: [100 * (0 - min) / (max - min)],
+        labels: [name],
+        stroke: {
+            dashArray: 3
+        },
+        plotOptions: {
+            radialBar: {
+                startAngle: -135,
+                endAngle: 135,
+                dataLabels: {
+                    name: {show: true, offsetY: -15},
+                    value: {
+                        show: true,
+                        offsetY: 0,
+                        fontSize: "30px",
+                        color: "#ccc",
+                        formatter: function(val) {
+                            return (min + (max - min) * val/100).toFixed(precision) + unit;
+                        },
+                    }
+                },
+                track: {show: true, opacity: 0.1},
+            }
+        },
     });
 
 }
@@ -96,35 +96,35 @@ function createRadialBarChart(name, unit, min, max, precision) {
 function createBarChart(name, unit, min, max) {
     return ({
         series: [
-	    {
-		name: name,
-		data: [50]
-	    }],
+            {
+                name: name,
+                data: [50]
+            }],
         chart: {
-	    type: 'bar',
-	    height: 8,
-	    width: "67%",
-	    toolbar: {show: false},
-	    sparkline: {enabled: true}
-	},
+            type: 'bar',
+            height: 8,
+            width: "67%",
+            toolbar: {show: false},
+            sparkline: {enabled: true}
+        },
         plotOptions: {
-	    bar: {
-		horizontal: true,
-		colors: {
-		    backgroundBarColors: ["#eee"],
-		    backgroundBarOpacity: 0.1
-		}
-	    }
-	},
+            bar: {
+                horizontal: true,
+                colors: {
+                    backgroundBarColors: ["#eee"],
+                    backgroundBarOpacity: 0.1
+                }
+            }
+        },
         dataLabels: {enabled: false},
         xaxis: {
-	    show: false,
-	    categories: [name],
-	    labels: {show: false},
-	    axisTicks: {show: false},
-	},
-	yaxis: {labels: {show: false}, min: min, max: max},
-	tooltip: {enabled: false}
+            show: false,
+            categories: [name],
+            labels: {show: false},
+            axisTicks: {show: false},
+        },
+        yaxis: {labels: {show: false}, min: min, max: max},
+        tooltip: {enabled: false}
     });
 }
 
@@ -141,9 +141,9 @@ function selectTimeline(activeElement) {
     // extract timeline from button ID
     var id = activeElement.target.id;
     if (id.indexOf("timeline_") >= 0) {
-	timeline = id.substring(9, id.length);
-	updateSeries(timeline);
-	currentTimeline = timeline;
+        timeline = id.substring(9, id.length);
+        updateSeries(timeline);
+        currentTimeline = timeline;
     }
 }
 
@@ -152,35 +152,35 @@ var hchart, cchart, tchart, pchart, schart;
 var temperature, humidity, pressure, cloudCoverage, sqm;
 
 var settings = {t_min: -40, t_max: 50, t_prec: 1,
-		p_min: 973, p_max: 1053, p_prec: 0,
-		sqm_min: 0, sqm_max: 25, sqm_prec: 1};
+                p_min: 973, p_max: 1053, p_prec: 0,
+                sqm_min: 0, sqm_max: 25, sqm_prec: 1};
 
 function init() {
 
     // add event listeners to buttons
     document.querySelector("#timeline_6h").
-	addEventListener('click', function (e) {selectTimeline(e);});
+        addEventListener('click', function (e) {selectTimeline(e);});
     document.querySelector("#timeline_1d").
-	addEventListener('click', function (e) {selectTimeline(e);});
+        addEventListener('click', function (e) {selectTimeline(e);});
     document.querySelector("#timeline_7d").
-	addEventListener('click', function (e) {selectTimeline(e);});
+        addEventListener('click', function (e) {selectTimeline(e);});
     document.querySelector("#timeline_30d").
-	addEventListener('click', function (e) {selectTimeline(e);});
+        addEventListener('click', function (e) {selectTimeline(e);});
 
 
     // create charts for current values
     temperature = new ApexCharts(document.querySelector("#temperature"),
-				 createRadialBarChart('Temperature', '°C',
-						      settings.t_min, settings.t_max, settings.t_prec));
+                                 createRadialBarChart('Temperature', '°C',
+                                                      settings.t_min, settings.t_max, settings.t_prec));
     humidity = new ApexCharts(document.querySelector("#humidity"),
-			      createBarChart('Humidity', '%', 0, 100));
+                              createBarChart('Humidity', '%', 0, 100));
     pressure = new ApexCharts(document.querySelector("#pressure"),
-			      createRadialBarChart('Pressure', ' hPa',
-						   settings.p_min, settings.p_max, settings.p_prec));
+                              createRadialBarChart('Pressure', ' hPa',
+                                                   settings.p_min, settings.p_max, settings.p_prec));
     cloudCoverage = new ApexCharts(document.querySelector("#clouds"),
-				   createRadialBarChart('Cloud Coverage', '%', 0, 100, 0));
+                                   createRadialBarChart('Cloud Coverage', '%', 0, 100, 0));
     sqm = new ApexCharts(document.querySelector("#sqm"),
-			      createBarChart('SQM', '%', settings.sqm_min, settings.sqm_max));
+                              createBarChart('SQM', '%', settings.sqm_min, settings.sqm_max));
     temperature.render();
     humidity.render();
     pressure.render();
@@ -194,15 +194,15 @@ function init() {
     // create the time series charts
     
     tchart = new ApexCharts(document.querySelector("#temperature_series"),
-			    createWeatherChart("Temperature", "°C", "left", undefined, 1));
+                            createWeatherChart("Temperature", "°C", "left", undefined, 1));
     hchart = new ApexCharts(document.querySelector("#humidity_series"),
-			    createWeatherChart("Humidity", "%", "left", 100, 0));
+                            createWeatherChart("Humidity", "%", "left", 100, 0));
     pchart = new ApexCharts(document.querySelector("#pressure_series"),
-			    createWeatherChart("Pressure", "hPa", "left", undefined, 0));
+                            createWeatherChart("Pressure", "hPa", "left", undefined, 0));
     cchart = new ApexCharts(document.querySelector("#clouds_series"),
-			    createWeatherChart("Cloud Coverage", "%", "left", 100, 0));
+                            createWeatherChart("Cloud Coverage", "%", "left", 100, 0));
     schart = new ApexCharts(document.querySelector("#sqm_series"),
-			    createWeatherChart("Sky Quality", "mag/arcsec²", "left", undefined, 1));
+                            createWeatherChart("Sky Quality", "mag/arcsec²", "left", undefined, 1));
 
     hchart.render();
     cchart.render();
@@ -211,9 +211,9 @@ function init() {
     schart.render();
 
     tchart.updateOptions({colors: ["#008fec", "#0077b3"],
-			  fill: {type: ['gradient', 'pattern'],
-				 pattern: {style: 'verticalLines'}},
-			  legend: {show: false}});
+                          fill: {type: ['gradient', 'pattern'],
+                                 pattern: {style: 'verticalLines'}},
+                          legend: {show: false}});
 
     updateSeries(currentTimeline);
 
@@ -225,39 +225,39 @@ function updateSeries(timeline) {
     // update last values
     $.get("data/RTdata_lastupdate.json", function(data) {
 
-	var currentTemperature   = data.Temperature;
-	var currentCloudCoverage = data.CloudCover;
-	var currentHumidity      = data.Humidity;
-	var currentPressure      = data.Pressure;
-	var currentSQM           = data.SQM;
+        var currentTemperature   = data.Temperature;
+        var currentCloudCoverage = data.CloudCover;
+        var currentHumidity      = data.Humidity;
+        var currentPressure      = data.Pressure;
+        var currentSQM           = data.SQM;
 
-	// calculate filling percentage from current temperature and pressure (slightly ugly code)
-	temperature.updateSeries([100 * (currentTemperature - settings.t_min) / (settings.t_max - settings.t_min)]);
-	pressure.updateSeries([100 * (currentPressure - settings.p_min) / (settings.p_max - settings.p_min)]);
-	cloudCoverage.updateSeries([currentCloudCoverage]);
-	humidity.updateSeries([{name: "Humidity", data: [currentHumidity]}]);
-	document.querySelector("#humidityValue").textContent = currentHumidity + "%";
-	sqm.updateSeries([{name: "SQM", data: [currentSQM]}]);
-	document.querySelector("#sqmValue").textContent = currentSQM.toFixed(1);
+        // calculate filling percentage from current temperature and pressure (slightly ugly code)
+        temperature.updateSeries([100 * (currentTemperature - settings.t_min) / (settings.t_max - settings.t_min)]);
+        pressure.updateSeries([100 * (currentPressure - settings.p_min) / (settings.p_max - settings.p_min)]);
+        cloudCoverage.updateSeries([currentCloudCoverage]);
+        humidity.updateSeries([{name: "Humidity", data: [currentHumidity]}]);
+        document.querySelector("#humidityValue").textContent = currentHumidity + "%";
+        sqm.updateSeries([{name: "SQM", data: [currentSQM]}]);
+        document.querySelector("#sqmValue").textContent = currentSQM.toFixed(1);
 
-	// update time stamp at the bottom line
-	var lastUpdate = data.timestamp;
-	document.querySelector("#lastupdate").textContent = new Date(lastUpdate).toLocaleString();
+        // update time stamp at the bottom line
+        var lastUpdate = data.timestamp;
+        document.querySelector("#lastupdate").textContent = new Date(lastUpdate).toLocaleString();
     });
 
 
     $.get("data/RTdata_" + timeline + ".json", function(data) {
 
-	hchart.updateSeries([data.Humidity]);
-	cchart.updateSeries([data.CloudCover]);
-	tchart.updateSeries([{data: data.Temperature.data,
-			      name: data.Temperature.name,
-			      type: "area"},
-			     {data: data.DewPoint.data,
-			      name: "Dew Point",
-			      type: "area"}]);
-	pchart.updateSeries([data.Pressure]);
-	schart.updateSeries([data.SQM]);
+        hchart.updateSeries([data.Humidity]);
+        cchart.updateSeries([data.CloudCover]);
+        tchart.updateSeries([{data: data.Temperature.data,
+                              name: data.Temperature.name,
+                              type: "area"},
+                             {data: data.DewPoint.data,
+                              name: "Dew Point",
+                              type: "area"}]);
+        pchart.updateSeries([data.Pressure]);
+        schart.updateSeries([data.SQM]);
     });
 
 };
