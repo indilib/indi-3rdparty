989acd14623e96718a81823534706a830078321a
Jasem Mutlaq <mutlaqja@ikarustech.com>
2020-02-17 23:34:43 +0300

    Camera background flushing should be ON by default. When turned off, warn the user

--- indi-fli/fli_ccd.h
+++ indi-fli/fli_ccd.h
@@ -31,91 +31,91 @@ using namespace std;
 
 class FLICCD : public INDI::CCD
 {
-  public:
-    FLICCD();
-    ~FLICCD();
+    public:
+        FLICCD();
+        virtual ~FLICCD() override;
 
-    const char *getDefaultName() override;
+        const char *getDefaultName() override;
 
-    bool initProperties() override;
-    void ISGetProperties(const char *dev) override;
-    bool updateProperties() override;
+        bool initProperties() override;
+        void ISGetProperties(const char *dev) override;
+        bool updateProperties() override;
 
-    bool Connect() override;
-    bool Disconnect() override;
+        bool Connect() override;
+        bool Disconnect() override;
 
-    bool StartExposure(float duration) override;
-    bool AbortExposure() override;
+        bool StartExposure(float duration) override;
+        bool AbortExposure() override;
 
-    virtual bool ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n) override;
-    virtual bool ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n) override;
+        virtual bool ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n) override;
+        virtual bool ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n) override;
 
-  protected:
-    virtual void TimerHit() override;
-    virtual int SetTemperature(double temperature) override;
-    virtual bool UpdateCCDFrame(int x, int y, int w, int h) override;
-    virtual bool UpdateCCDBin(int binx, int biny) override;
-    virtual bool UpdateCCDFrameType(INDI::CCDChip::CCD_FRAME fType) override;
+    protected:
+        virtual void TimerHit() override;
+        virtual int SetTemperature(double temperature) override;
+        virtual bool UpdateCCDFrame(int x, int y, int w, int h) override;
+        virtual bool UpdateCCDBin(int binx, int biny) override;
+        virtual bool UpdateCCDFrameType(INDI::CCDChip::CCD_FRAME fType) override;
 
-    virtual void debugTriggered(bool enable) override;
-    virtual bool saveConfigItems(FILE *fp) override;
+        virtual void debugTriggered(bool enable) override;
+        virtual bool saveConfigItems(FILE *fp) override;
 
-  private:
-    // Find FLI CCD
-    bool findFLICCD(flidomain_t domain);
+    private:
+        // Find FLI CCD
+        bool findFLICCD(flidomain_t domain);
 
-    // Calculate Time until exposure is complete
-    float calcTimeLeft();
+        // Calculate Time until exposure is complete
+        float calcTimeLeft();
 
-    // Fetch image row by row from the CCD
-    bool grabImage();
+        // Fetch image row by row from the CCD
+        bool grabImage();
 
-    // Get initial CCD values upon connection
-    bool setupParams();
+        // Get initial CCD values upon connection
+        bool setupParams();
 
-    typedef struct
-    {
-        flidomain_t domain;
-        char *dname;
-        char *name;
-        char model[32]={0};
-        long HWRevision;
-        long FWRevision;
-        double x_pixel_size;
-        double y_pixel_size;
-        long Array_Area[4];
-        long Visible_Area[4];
-        int width, height;
-        double temperature;
-    } cam_t;
+        typedef struct
+        {
+            flidomain_t domain;
+            char *dname;
+            char *name;
+            char model[32] = {0};
+            long HWRevision;
+            long FWRevision;
+            double x_pixel_size;
+            double y_pixel_size;
+            long Array_Area[4];
+            long Visible_Area[4];
+            int width, height;
+            double temperature;
+        } cam_t;
 
-    ISwitch PortS[4];
-    ISwitchVectorProperty PortSP;
+        ISwitch PortS[4];
+        ISwitchVectorProperty PortSP;
 
-    IText CamInfoT[3] {};
-    ITextVectorProperty CamInfoTP;
+        IText CamInfoT[3] {};
+        ITextVectorProperty CamInfoTP;
 
-    INumber CoolerN[1];
-    INumberVectorProperty CoolerNP;
+        INumber CoolerN[1];
+        INumberVectorProperty CoolerNP;
 
-    INumber FlushN[1];
-    INumberVectorProperty FlushNP;
+        INumber FlushN[1];
+        INumberVectorProperty FlushNP;
 
-    ISwitch BackgroundFlushS[2];
-    ISwitchVectorProperty BackgroundFlushSP;
+        ISwitch BackgroundFlushS[2];
+        ISwitchVectorProperty BackgroundFlushSP;
 
-    ISwitch *CameraModeS = nullptr;
-    ISwitchVectorProperty CameraModeSP;
+        ISwitch *CameraModeS = nullptr;
+        ISwitchVectorProperty CameraModeSP;
 
-    int timerID=0;
+        int timerID = 0;
 
-    // Exposure timing
-    struct timeval ExpStart;
-    float ExposureRequest;
+        // Exposure timing
+        struct timeval ExpStart;
+        float ExposureRequest {0};
 
-    flidev_t fli_dev;
-    cam_t FLICam;
+        flidev_t fli_dev;
+        cam_t FLICam;
 
-    // Simulation mode
-    bool sim=false;
+        // Simulation mode
+        bool sim = false;
 };
