3a1198fccf433305f87e4d0c59f843f0a1ce239e
Eric Dejouhanet <eric.dejouhanet@gmail.com>
2020-03-03 22:06:23 +0100

    Save horizon configuration items.

--- indi-eqmod/eqmodbase.cpp
+++ indi-eqmod/eqmodbase.cpp
@@ -3674,6 +3674,10 @@ bool EQMod::saveConfigItems(FILE *fp)
     if (align)
         align->saveConfigItems(fp);
 #endif
+#ifdef WITH_SCOPE_LIMITS
+    if (horizon)
+        horizon->saveConfigItems(fp);
+#endif
     return true;
 }
 
