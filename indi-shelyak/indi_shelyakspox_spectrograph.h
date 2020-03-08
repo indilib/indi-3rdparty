f6b6235e33137445b650de854daa17352f24fd21
BUTET <ashashiwa@gmail.com>
2020-03-08 18:50:30 +0100

    add sky button and cleaning (#67)

--- indi-shelyak/indi_shelyakspox_spectrograph.h
+++ indi-shelyak/indi_shelyakspox_spectrograph.h
@@ -39,7 +39,7 @@ std::map<ISState, char> COMMANDS = {
   {ISS_ON, 0x31}, {ISS_OFF, 0x30}    //"1" and "0"
 };
 std::map<std::string, char> PARAMETERS = {
-  {"CALIBRATION", 0x31}, {"FLAT", 0x32}, {"DARK", 0x33} //"1", "2", "3"
+  {"SKY", 0x30}, {"CALIBRATION", 0x31}, {"FLAT", 0x32}, {"DARK", 0x33} //"0","1", "2", "3"
 };
 
 class ShelyakSpox : public INDI::DefaultDevice
@@ -66,7 +66,7 @@ private:
 
   // Main Control
   ISwitchVectorProperty LampSP;
-  ISwitch LampS[3];
+  ISwitch LampS[4];
 
   // Options
   ITextVectorProperty PortTP;
@@ -75,13 +75,9 @@ private:
   // Spectrograph Settings
   INumberVectorProperty SettingsNP;
   INumber SettingsN[2];
-
-  const char* lastLampOn;
-  
   
   bool calibrationUnitCommand(char command, char parameter);
   bool resetLamps();
-  //bool pollingLamps();
 };
 
 #endif // SHELYAKSPOX_SPECTROGRAPH_H
