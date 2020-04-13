38d474972eecb8d73a230c3575fd467c7305adba
Ilia Platone <info@iliaplatone.com>
2020-04-14 01:34:17 +0200

    [AHP Correlator] Moved into AHP Interferometer

--- indi-ahp-interferometer/indi_interferometer.xml.cmake
+++ indi-ahp-interferometer/indi_interferometer.xml.cmake
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <driversList>
 <devGroup group="CCDs">
-        <device label="Telescope array" mdpd="true">
-                <driver name="Telescope array">indi_interferometer</driver>
+        <device label="AHP Telescope array" mdpd="true">
+                <driver name="AHP Telescope array correlator">indi_ahp_interferometer</driver>
                 <version>@INTERFEROMETER_VERSION_MAJOR@.@INTERFEROMETER_VERSION_MINOR@</version>
 	</device>
 </devGroup>
