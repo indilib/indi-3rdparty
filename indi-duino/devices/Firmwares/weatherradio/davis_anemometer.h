0e36246e1cd22ee226510c2bb6b59745914e4f2d
Wolfgang Reissenberger <sterne-jaeger@t-online.de>
2020-05-08 09:05:20 +0200

    Weather Radio | Bug fix in wind speed calculation if no wind is present at all

--- indi-duino/devices/Firmwares/weatherradio/davis_anemometer.h
+++ indi-duino/devices/Firmwares/weatherradio/davis_anemometer.h
@@ -49,7 +49,10 @@ float windspeed(unsigned long time, unsigned long startTime, unsigned int rotati
   // equals 1 mp/h wind speed (1 mp/h = 1609/3600 m/s)
   // speed (m/s) = rotations * 1135.24 / delta t
 
-  return (rotations * 1135.24 / (time - startTime));
+  if (time == startTime)
+    return 0.0;
+  else
+    return (rotations * 1135.24 / (time - startTime));
 }
 
 // calculate the wind direction in degree (N = 0, E = 90, ...)
@@ -126,10 +129,6 @@ void initAnemometer() {
   anemometerData.status = true;
   // reset measuring data
   reset(millis());
-  anemometerData.avgSpeed = 0;
-  anemometerData.minSpeed = 0;
-  anemometerData.maxSpeed = 0;
-  anemometerData.rotations = 0;
 }
 
 void updateAnemometer() {
