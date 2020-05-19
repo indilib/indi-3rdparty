fb6649ca6e94f3887b2f7a771bec6d0aaec42e43
Ilia Platone <info@iliaplatone.com>
2020-05-19 05:38:27 +0200

    Added beta version (really draft) of RTKLIB driver

---
+++ indi-rtklib/config.h.cmake
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Define INDI Data Dir */
+#cmakedefine INDI_DATA_DIR "@INDI_DATA_DIR@"
+/* Define Driver version */
+#define RTKLIB_VERSION_MAJOR @RTKLIB_VERSION_MAJOR@
+#define RTKLIB_VERSION_MINOR @RTKLIB_VERSION_MINOR@
+
+#endif // CONFIG_H
---
+++ indi-rtklib/indi_rtklib.xml.cmake
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="Auxiliary">
+   <device label="RTKLIB Precise Positioning">
+      <driver name="RTKLIB Precise Positioning">indi_rtklib</driver>
+      <version>@RTKLIB_VERSION_MAJOR@.@RTKLIB_VERSION_MINOR@</version>
+   </device>
+</devGroup>
+</driversList>
