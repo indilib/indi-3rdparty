cc837ff361b32758ccf0fbcd4efad552e6938520
Jasem Mutlaq <mutlaqja@ikarustech.com>
2020-06-06 10:15:24 +0300

    Make Pentax label Native and fix driver name

--- indi-pentax/indi_pentax.xml.cmake
+++ indi-pentax/indi_pentax.xml.cmake
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <driversList>
 <devGroup group="CCDs">
-        <device label="Pentax DSLR (Native)" mdpd="true">
-                <driver name="Pentax DSLR (Native)">indi_pentax</driver>
+        <device label="Pentax DSLR Native" mdpd="true">
+                <driver name="Pentax DSLR">indi_pentax</driver>
                 <version>@INDI_PENTAX_VERSION_MAJOR@.@INDI_PENTAX_VERSION_MINOR@</version>
-	</device>
+        </device>
 </devGroup>
 </driversList>
