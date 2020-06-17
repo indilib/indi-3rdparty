64cdb18e17cfe4da494a7bbf16f4fb3c5c3eb8d2
Jasem Mutlaq <mutlaqja@ikarustech.com>
2020-06-17 17:48:12 +0300

    Add Fuji to list of cameras without view finder

--- indi-gphoto/gphoto_driver.cpp
+++ indi-gphoto/gphoto_driver.cpp
@@ -2148,8 +2148,10 @@ int gphoto_capture_preview(gphoto_driver *gphoto, CameraFile *previewFile, char 
 
 int gphoto_start_preview(gphoto_driver *gphoto)
 {
-    // Olympus & Sony cameras support streaming but without viewfinder_widget
-    if (strcasestr(gphoto->manufacturer, "OLYMPUS") || strcasestr(gphoto->manufacturer, "Sony Corporation"))
+    // Olympus, Sony, and Fuji cameras support streaming but without viewfinder_widget
+    if (strcasestr(gphoto->manufacturer, "OLYMPUS") ||
+            strcasestr(gphoto->manufacturer, "Sony Corporation") ||
+            strcasestr(gphoto->manufacturer, "Fuji"))
         return GP_OK;
 
     // If viewfinder not found, nothing to do
