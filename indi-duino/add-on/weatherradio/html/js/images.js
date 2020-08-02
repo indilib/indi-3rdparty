5f082e3bd292d2ea50f918457898d8f5c76e1586
Wolfgang Reissenberger <sterne-jaeger@t-online.de>
2020-08-02 19:14:37 +0200

    Weather Radio | Carousel replaced by light bar with modal display

--- indi-duino/add-on/weatherradio/html/js/images.js
+++ indi-duino/add-on/weatherradio/html/js/images.js
@@ -1,9 +1,30 @@
 function loadImages() {
-    updateCarousel();
+    updateImageBar();
+    // updateCarousel();
     // update images every 5 min
-    setInterval( function(){ updateCarousel();}, 5*60000);
+    setInterval( function(){ updateImageBar();}, 5*60000);
 }
 
+function openLightbox(filename) {
+    lightboxContent = document.getElementById("lightbox-content");
+    // clear the lightbox, remove existing DOM children
+    for (child of lightboxContent.children)
+	lightboxContent.removeChild(child);
+
+    // add selected image
+    var img = document.createElement("img");
+    img.setAttribute("id", filename);
+    img.setAttribute("src", "./media/" + filename);
+    img.setAttribute("width", "100%");
+    lightboxContent.appendChild(img);
+
+    // show modal
+    document.getElementById("lightbox").style.display = "block";
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
 
 function updateCarousel() {
     $.get("data/images.json", function(files) {
@@ -46,6 +67,29 @@ function updateCarousel() {
 		li.setAttribute("class", "active");
 		div.setAttribute("class", "carousel-item active");
 	    }
+	}
+
+    });
+};
+
+function updateImageBar() {
+    $.get("data/images.json", function(files) {
+	imageBar = document.querySelector("#image_bar");
+	// clear the image bar, remove existing DOM children
+	for (child of imageBar.children)
+	    imageBar.removeChild(child);
+
+	for (file of files) {
+	    // images
+	    var img = document.createElement("img");
+	    img.setAttribute("id", file.name);
+	    img.setAttribute("src", "./media/" + file.name);
+	    img.setAttribute("width", "96");
+	    // open modal dialog
+	    img.setAttribute("onclick", "openLightbox(\"" + file.name + "\")");
+	    img.setAttribute("class", "hover-shadow");
+
+	    imageBar.appendChild(img);
 	}
 
     });
