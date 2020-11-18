d4d0322fece61fb02e7c19304214dd91b5ccbb03
Ilia Platone <info@iliaplatone.com>
2020-11-18 17:15:50 +0100

    Updated FindAHPXC.cmake

--- cmake_modules/FindAHPXC.cmake
+++ cmake_modules/FindAHPXC.cmake
@@ -8,19 +8,13 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-if (AHPXC_INCLUDE_DIR AND AHPXC_LIBRARIES)
+if (AHPXC_LIBRARIES)
 
       # in cache already
       set(AHPXC_FOUND TRUE)
       message(STATUS "Found libqsiapi: ${AHPXC_LIBRARIES}")
 
-    else (AHPXC_INCLUDE_DIR AND AHPXC_LIBRARIES)
-
-      find_path(AHPXC_INCLUDE_DIR ahp_xc.h
-        PATH_SUFFIXES ahp
-        ${_obIncDir}
-        ${GNUWIN32_DIR}/include
-      )
+else (AHPXC_LIBRARIES)
 
       find_library(AHPXC_LIBRARIES NAMES ahp_xc
         PATHS
@@ -28,11 +22,11 @@ if (AHPXC_INCLUDE_DIR AND AHPXC_LIBRARIES)
         ${GNUWIN32_DIR}/lib
       )
 
-      if(AHPXC_INCLUDE_DIR AND AHPXC_LIBRARIES)
+      if(AHPXC_LIBRARIES)
         set(AHPXC_FOUND TRUE)
-      else (AHPXC_INCLUDE_DIR AND AHPXC_LIBRARIES)
+      else (AHPXC_LIBRARIES)
         set(AHPXC_FOUND FALSE)
-      endif(AHPXC_INCLUDE_DIR AND AHPXC_LIBRARIES)
+      endif(AHPXC_LIBRARIES)
 
 
       if (AHPXC_FOUND)
@@ -45,6 +39,6 @@ if (AHPXC_INCLUDE_DIR AND AHPXC_LIBRARIES)
         endif (AHPXC_FIND_REQUIRED)
       endif (AHPXC_FOUND)
 
-      mark_as_advanced(AHPXC_INCLUDE_DIR AHPXC_LIBRARIES)
+      mark_as_advanced(AHPXC_LIBRARIES)
 
-    endif (AHPXC_INCLUDE_DIR AND AHPXC_LIBRARIES)
+endif (AHPXC_LIBRARIES)
