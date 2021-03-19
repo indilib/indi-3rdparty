7538aac15c4d10d65221040b0b543d0552e720b7
Pawel Soja <kernel32.pl@gmail.com>
2021-03-19 12:37:43 +0100

    Add toString functions, format document.

--- indi-asi/singleworker_tmp.h
+++ indi-asi/singleworker_tmp.h
@@ -1,3 +1,20 @@
+/*
+    Copyright (C) 2021 Pawel Soja (kernel32.pl@gmail.com)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 #pragma once
 
 #include <functional>
@@ -34,25 +51,16 @@ public:
     }
 public:
     bool isAboutToQuit() const
-    {
-        return mIsAboutToQuit;
-    }
+    { return mIsAboutToQuit; }
 
     bool isRunning() const
-    {
-        return mIsRunning;
-    }
+    { return mIsRunning; }
 
 public:
     void quit()
     {
         std::lock_guard<std::mutex> lock(runLock);
         mIsAboutToQuit = true;
-    }
-
-    void wait()
-    {
-        std::lock_guard<std::mutex> lock(runLock);
         if (thread.joinable())
             thread.join();
     }
