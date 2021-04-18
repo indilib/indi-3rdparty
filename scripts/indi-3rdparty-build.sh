afa651338b6e93fa061b5c6b9961ba8b605f6a5d
Pawel Soja <kernel32.pl@gmail.com>
2021-04-18 23:07:40 +0200

    Enable FIX_WARNINGS.

--- scripts/indi-3rdparty-build.sh
+++ scripts/indi-3rdparty-build.sh
@@ -8,7 +8,7 @@ mkdir -p build/indi-3rdparty
 pushd build/indi-3rdparty
 cmake \
     -DCMAKE_INSTALL_PREFIX=/usr/local \
-    -DFIX_WARNINGS=OFF \
+    -DFIX_WARNINGS=ON \
     -DCMAKE_BUILD_TYPE=$1 \
     -DINDI_BUILD_UNITTESTS=ON \
     -DBUILD_LIBS=0 \
