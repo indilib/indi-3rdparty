8dfc38c47387b83b707ca9cd858d5e0563728aff
Wolfgang Reissenberger <sterne-jaeger@t-online.de>
2021-05-11 17:32:52 +0200

    Weather Radio | Ignore additional input lines on serial interface

--- indi-duino/weatherradio.cpp
+++ indi-duino/weatherradio.cpp
@@ -1334,6 +1334,12 @@ bool WeatherRadio::sendQuery(const char* cmd, char* response, int *length)
     // communication through a serial (USB) interface
     if (getActiveConnection()->type() == Connection::Interface::CONNECTION_SERIAL)
     {
+        // first clear the read buffer for unexpected data
+        char tmp_response[MAX_WEATHERBUFFER] = {0};
+        int tmp_bytes = 0;
+        while (receiveSerial(tmp_response, &tmp_bytes, '\n', 0))
+            tmp_response[0] = '\0';
+        // send query
         char cmdstring[20] = {0};
         sprintf(cmdstring, "%s\n", cmd);
         LOGF_DEBUG("Sending query: %s", cmdstring);
