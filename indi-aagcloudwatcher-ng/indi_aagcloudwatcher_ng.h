d551dd5c2b5122358b3898a068a7112cfcc4c2b1
Jasem Mutlaq <mutlaqja@ikarustech.com>
2021-06-30 03:40:53 +0300

    Make code less convoluted than before. Still needs a lot more refactoring

--- indi-aagcloudwatcher-ng/indi_aagcloudwatcher_ng.h
+++ indi-aagcloudwatcher-ng/indi_aagcloudwatcher_ng.h
@@ -77,4 +77,22 @@ class AAGCloudWatcher : public INDI::Weather
         float globalRainSensorHeater;
 
         double m_FirmwareVersion {5};
+
+        enum
+        {
+            SENSOR_SUPPLY,
+            SENSOR_SKY,
+            SENSOR_SENSOR,
+            SENSOR_AMBIENT,
+            SENSOR_RAIN,
+            SENSOR_RAIN_HEATER,
+            SENSOR_RAIN_TEMPERATURE,
+            SENSOR_LDR,
+            SENSOR_READ_CYCLES,
+            SENSOR_WIND_SPEED,
+            SENSOR_RELATIVE_HUMIDITY,
+            SENSOR_PRESSURE,
+            SENSOR_TOTAL_READINGS
+        };
+
 };
