dd36e6e7a2f08d03f821e62cde8887ccdffc6117
Jasem Mutlaq <mutlaqja@ikarustech.com>
2021-11-10 21:09:43 +0300

    Mark older cloudwatcher as obsolete

---
+++ obsolete/indi-aagcloudwatcher/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Define INDI Data Dir */
+#define INDI_DATA_DIR "/usr/share/indi"
+/* Define Driver version */
+#define AAG_VERSION_MAJOR 1
+#define AAG_VERSION_MINOR 4
+
+#endif // CONFIG_H
