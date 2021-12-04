33b352da1fb33ee9134fc6ac83f20cb7921083b2
Thorsten Alteholz <debian@alteholz.de>
2021-12-04 09:51:20 +0100

    fix spelling (#495)

--- indi-eqmod/eqmodbase.cpp
+++ indi-eqmod/eqmodbase.cpp
@@ -945,7 +945,7 @@ bool EQMod::ReadScopeStatus()
                        AlignedDEString,
                        AZString,
                        ALString,
-                       pierSide == PIER_EAST ? "East" : (pierSide == PIER_WEST ? "West" : "Uknown"));
+                       pierSide == PIER_EAST ? "East" : (pierSide == PIER_WEST ? "West" : "Unknown"));
         }
 
         if (mount->HasAuxEncoders())
