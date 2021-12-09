3a2f10b8b290eadc4ae562a56a6f0d8cef16ef45
Jasem Mutlaq <mutlaqja@ikarustech.com>
2021-12-09 12:54:51 +0300

    Initial work for Celestron AUX refactor

--- indi-celestronaux/celestronaux.h
+++ indi-celestronaux/celestronaux.h
@@ -26,9 +26,14 @@
 #include <indicom.h>
 #include <indiguiderinterface.h>
 #include <inditelescope.h>
+#include <indielapsedtimer.h>
 #include <connectionplugins/connectionserial.h>
 #include <connectionplugins/connectiontcp.h>
 #include <alignment/AlignmentSubsystemForDrivers.h>
+#include <indipropertyswitch.h>
+#include <indipropertynumber.h>
+#include <indipropertytext.h>
+#include <termios.h>
 
 #include "auxproto.h"
 
@@ -41,21 +46,59 @@ class CelestronAUX :
         CelestronAUX();
         ~CelestronAUX();
 
-        virtual void ISGetProperties(const char *dev) override;
         virtual bool ISNewBLOB(const char *dev, const char *name, int sizes[], int blobsizes[], char *blobs[],
                                char *formats[], char *names[], int n) override;
         virtual bool ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n) override;
         virtual bool ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n) override;
         virtual bool ISNewText(const char *dev, const char *name, char *texts[], char *names[], int n) override;
         virtual bool ISSnoopDevice(XMLEle *root) override;
-        long requestedCordwrapPos;
-        double getNorthAz();
+
+        // Type defs
+        enum ScopeStatus_t
+        {
+            IDLE,
+            SLEWING_FAST,
+            APPROACH,
+            SLEWING_SLOW,
+            SLEWING_MANUAL,
+            TRACKING
+        };
+        ScopeStatus_t ScopeStatus;
+
+        enum AxisStatus
+        {
+            STOPPED,
+            SLEWING
+        };
+
+        enum AxisDirection
+        {
+            FORWARD,
+            REVERSE
+        };
+
+        // Previous motion direction
+        // TODO: Switch to AltAz from N-S/W-E
+        typedef enum
+        {
+            PREVIOUS_NS_MOTION_NORTH   = DIRECTION_NORTH,
+            PREVIOUS_NS_MOTION_SOUTH   = DIRECTION_SOUTH,
+            PREVIOUS_NS_MOTION_UNKNOWN = -1
+        } PreviousNSMotion_t;
+        typedef enum
+        {
+            PREVIOUS_WE_MOTION_WEST    = DIRECTION_WEST,
+            PREVIOUS_WE_MOTION_EAST    = DIRECTION_EAST,
+            PREVIOUS_WE_MOTION_UNKNOWN = -1
+        } PreviousWEMotion_t;
+
+        // Public to enable setting from ISSnoop
+        void syncCoordWrapPosition();
 
     protected:
         virtual bool initProperties() override;
         virtual bool updateProperties() override;
         virtual bool saveConfigItems(FILE *fp) override;
-        //virtual bool Connect() override;
         virtual bool Handshake() override;
         virtual bool Disconnect() override;
 
@@ -84,120 +127,77 @@ class CelestronAUX :
 
         virtual bool ReadScopeStatus() override;
         virtual void TimerHit() override;
-
         virtual bool updateLocation(double latitude, double longitude, double elevation) override;
 
-        bool trackingRequested();
-
-        int32_t GetALT();
-        int32_t GetAZ();
-        bool slewing();
-        bool Slew(AUXTargets trg, int rate);
+        /////////////////////////////////////////////////////////////////////////////////////
+        /// Motion Control
+        /////////////////////////////////////////////////////////////////////////////////////
+        bool isSlewing();
+        bool Slew(AUXTargets target, int rate);
         bool SlewALT(int32_t rate);
         bool SlewAZ(int32_t rate);
         bool GoToFast(int32_t alt, int32_t az, bool track);
         bool GoToSlow(int32_t alt, int32_t az, bool track);
-        bool setCordwrap(bool enable);
-        bool getCordwrap();
-    public:
-        bool setCordwrapPos(int32_t pos);
-        long getCordwrapPos();
-        bool getCWBase();
-    private:
-        bool getVersion(AUXTargets trg);
-        void getVersions();
+
+        /////////////////////////////////////////////////////////////////////////////////////
+        /// Tracking
+        /////////////////////////////////////////////////////////////////////////////////////
         bool Track(int32_t altRate, int32_t azRate);
         bool SetTrackEnabled(bool enabled) override;
+        bool trackingRequested();
+
+        /////////////////////////////////////////////////////////////////////////////////////
+        /// Coord Wrap
+        /////////////////////////////////////////////////////////////////////////////////////
+        bool setCordWrapEnabled(bool enable);
+        bool getCordWrapEnabled();
+        bool setCordWrapPosition(int32_t pos);
+        uint32_t getCordWrapPosition();
+
+private:
+        /////////////////////////////////////////////////////////////////////////////////////
+        /// Misc
+        /////////////////////////////////////////////////////////////////////////////////////
+        double getNorthAz();
+        bool getVersion(AUXTargets target);
+        void getVersions();
+        void hex_dump(char *buf, AUXBuffer data, size_t size);
+        int32_t clampStepsPerRevolution(int32_t);
+
+        /////////////////////////////////////////////////////////////////////////////////////
+        /// Guiding
+        /////////////////////////////////////////////////////////////////////////////////////
         bool TimerTick(double dt);
         bool GuidePulse(INDI_EQ_AXIS axis, uint32_t ms, int8_t rate);
 
 
-    private:
-
-        // mount type
-        MountType_t requestedMountType;
-        MountType_t currentMountType;
-
-        bool pastAlignmentSubsystemStatus = true;
-        bool currentAlignmentSubsystemStatus;
-
-        enum ScopeStatus_t
-        {
-            IDLE,
-            SLEWING_FAST,
-            APPROACH,
-            SLEWING_SLOW,
-            SLEWING_MANUAL,
-            TRACKING
-        };
-        ScopeStatus_t ScopeStatus;
-
-        enum AxisStatus
-        {
-            STOPPED,
-            SLEWING
-        };
-
-        enum AxisDirection
-        {
-            FORWARD,
-            REVERSE
-        };
-
+    private:        
+        // Axis Information
         AxisStatus AxisStatusALT;
         AxisDirection AxisDirectionALT;
-
         AxisStatus AxisStatusAZ;
         AxisDirection AxisDirectionAZ;
 
-        double Approach; // approach distance
+        // Motion Control
 
-        // Previous motion direction
-        // TODO: Switch to AltAz from N-S/W-E
-        typedef enum
-        {
-            PREVIOUS_NS_MOTION_NORTH   = DIRECTION_NORTH,
-            PREVIOUS_NS_MOTION_SOUTH   = DIRECTION_SOUTH,
-            PREVIOUS_NS_MOTION_UNKNOWN = -1
-        } PreviousNSMotion_t;
-        typedef enum
-        {
-            PREVIOUS_WE_MOTION_WEST    = DIRECTION_WEST,
-            PREVIOUS_WE_MOTION_EAST    = DIRECTION_EAST,
-            PREVIOUS_WE_MOTION_UNKNOWN = -1
-        } PreviousWEMotion_t;
-
-        // GoTo
-        INDI::IEquatorialCoordinates GoToTarget;
-        int slewTicks, maxSlewTicks;
+        // approach distance
+        double Approach;
 
         // Tracking
-        INDI::IEquatorialCoordinates CurrentTrackingTarget;
-        INDI::IEquatorialCoordinates NewTrackingTarget;
+        INDI::IEquatorialCoordinates m_SkyTrackingTarget { 0, 0 };
+        INDI::IEquatorialCoordinates m_SkyCurrentRADE {0, 0};
+        INDI::IHorizontalCoordinates m_MountAltAz {0, 0};
+        long OldTrackingTarget[2] { 0, 0 };
+        INDI::ElapsedTimer m_TrackingElapsedTimer;
 
-        // Tracing in timer tick
-        int TraceThisTickCount;
-        bool TraceThisTick;
 
-        // connection
-        bool isRTSCTS;
-        bool isHC;
-
-        uint32_t DBG_CAUX {0};
-        uint32_t DBG_SERIAL {0};
-
-        int32_t range360int(int32_t);
-        void initScope(char const *ip, int port);
-        void initScope();
-        bool detectNetScope(bool set_ip);
-        bool detectNetScope();
         void closeConnection();
         void emulateGPS(AUXCommand &m);
         bool serialReadResponse(AUXCommand c);
         bool tcpReadResponse();
         bool readAUXResponse(AUXCommand c);
         bool processResponse(AUXCommand &cmd);
-        void querryStatus();
+        void queryStatus();
         int sendBuffer(int PortFD, AUXBuffer buf);
         bool sendAUXCommand(AUXCommand &c);
         void formatVersionString(char *s, int n, uint8_t *verBuf);
@@ -215,13 +215,10 @@ class CelestronAUX :
         // FIXME: Combined slew rate?
         int32_t slewRate {0};
 
-        bool m_Tracking {false};
         bool m_SlewingAlt {false}, m_SlewingAz {false};
-        bool gpsemu;
-        bool cw_base_sky = false ;
+        bool m_GPSEmulation {false};
 
-
-
+        // Firmware
         uint8_t m_MainBoardVersion[4] {0};
         uint8_t m_AltitudeVersion[4] {0};
         uint8_t m_AzimuthVersion[4] {0};
@@ -233,8 +230,15 @@ class CelestronAUX :
         // Coord Wrap
         bool m_CordWrapActive {false};
         int32_t m_CordWrapPosition {0};
+        uint32_t m_RequestedCordwrapPos;
 
-        // FP
+        // Debug
+        uint32_t DBG_CAUX {0};
+        uint32_t DBG_SERIAL {0};
+
+        ///////////////////////////////////////////////////////////////////////////////
+        /// Communication
+        ///////////////////////////////////////////////////////////////////////////////
         int modem_ctrl;
         void setRTS(bool rts);
         bool waitCTS(float timeout);
@@ -244,59 +248,58 @@ class CelestronAUX :
         int aux_tty_read(int PortFD, char *buf, int bufsiz, int timeout, int *n);
         int aux_tty_write (int PortFD, char *buf, int bufsiz, float timeout, int *n);
         bool tty_set_speed(int PortFD, speed_t speed);
-        void hex_dump(char *buf, AUXBuffer data, size_t size);
 
+        // connection
+        bool m_IsRTSCTS;
+        bool m_isHandController;
 
         ///////////////////////////////////////////////////////////////////////////////
         /// Celestron AUX Properties
         ///////////////////////////////////////////////////////////////////////////////
 
         // Firmware
-        IText FirmwareT[10] {};
-        ITextVectorProperty FirmwareTP;
+        INDI::PropertyText FirmwareTP {7};
         enum {FW_HC, FW_MB, FW_AZM, FW_ALT, FW_WiFi, FW_BAT, FW_GPS};
-        // Networked Mount autodetect
-        ISwitch NetDetectS[1];
-        ISwitchVectorProperty NetDetectSP;
         // Mount type
-        ISwitch MountTypeS[2];
-        ISwitchVectorProperty MountTypeSP;
+        INDI::PropertySwitch MountTypeSP {2};
         enum
         {
             MOUNT_EQUATORIAL,
             MOUNT_ALTAZ
         };
-        // Mount Cordwrap
-        ISwitch CordWrapS[2];
-        ISwitchVectorProperty CordWrapSP;
+
+        // Mount Cord wrap Toogle
+        INDI::PropertySwitch CordWrapToggleSP {2};
         enum { CORDWRAP_OFF, CORDWRAP_ON };
-        ISwitch CWPosS[4];
-        ISwitchVectorProperty CWPosSP;
+
+        // Mount Coord wrap Position
+        INDI::PropertySwitch CordWrapPositionSP {4};
         enum { CORDWRAP_N, CORDWRAP_E, CORDWRAP_S, CORDWRAP_W };
+
         // Cordwrap base (0-encoder/True directions)
-        ISwitch CWBaseS[2];
-        ISwitchVectorProperty CWBaseSP;
-        enum {CW_BASE_ENC, CW_BASE_SKY}; // Use 0-encoders / Sky directions as base for parking and cordwrap
+        // Use 0-encoders / Sky directions as base for parking and cordwrap
+        INDI::PropertySwitch CordWrapBaseSP {2};
+        enum {CW_BASE_ENC, CW_BASE_SKY};
 
         // GPS emulator
-        ISwitch GPSEmuS[2];
-        ISwitchVectorProperty GPSEmuSP;
+        INDI::PropertySwitch GPSEmuSP {2};
         enum { GPSEMU_OFF, GPSEMU_ON };
-        // guide
-        INumber GuideRateN[2] {};
-        INumberVectorProperty GuideRateNP;
+
+        // Guide Rate
+        INDI::PropertyNumber GuideRateNP {2};
 
         ///////////////////////////////////////////////////////////////////////////////
         /// Static Const Private Variables
         ///////////////////////////////////////////////////////////////////////////////
 
+    private:
+
         // One definition rule (ODR) constants
         // AUX commands use 24bit integer as a representation of angle in units of
         // fractional revolutions. Thus 2^24 steps makes full revolution.
         static constexpr int32_t STEPS_PER_REVOLUTION {16777216};
-    public:
         static constexpr double STEPS_PER_DEGREE {STEPS_PER_REVOLUTION / 360.0};
-    private:
+
         static constexpr double DEFAULT_SLEW_RATE {STEPS_PER_DEGREE * 2.0};
         static constexpr double MAX_ALT {90.0 * STEPS_PER_DEGREE};
         static constexpr double MIN_ALT {-90.0 * STEPS_PER_DEGREE};
@@ -308,10 +311,6 @@ class CelestronAUX :
         // to 61440/STEPS_PER_DEGREE = 1.318359375.
         static constexpr double TRACK_SCALE {61440 / STEPS_PER_DEGREE};
         static constexpr double SIDERAL_RATE {1.002737909350795};
-        static constexpr uint8_t MAX_SLEW_RATE {9};
-        static constexpr uint8_t FIND_SLEW_RATE {7};
-        static constexpr uint8_t CENTERING_SLEW_RATE {3};
-        static constexpr uint8_t GUIDE_SLEW_RATE {2};
         static constexpr uint32_t BUFFER_SIZE {10240};
         // seconds
         static constexpr uint8_t READ_TIMEOUT {1};
@@ -319,5 +318,8 @@ class CelestronAUX :
         static constexpr uint8_t CTS_TIMEOUT {100};
         // ms
         static constexpr uint8_t RTS_DELAY {50};
+        // Coord Wrap
+        static constexpr const char *CORDWRAP_TAB {"Coord Wrap"};
+        static constexpr const char *MOUNTINFO_TAB {"Mount info"};
 
 };
