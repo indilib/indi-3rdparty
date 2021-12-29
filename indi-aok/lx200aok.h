44013a8727e4342715f72f9b441b0f7cf51bb54e
escribana <escriban@bluewin.ch>
2021-12-29 15:23:32 +0100

    Driver Version 2.0: refactor and overhaul of firmware 21xxxxxx for Skywalker Teleskop Controller (#513)

--- indi-aok/lx200aok.h
+++ indi-aok/lx200aok.h
@@ -31,13 +31,15 @@
 
 #include <cstring>
 #include <string>
+// #include <regex> --> std::map
 #include <unistd.h>
 
-#define LX200_TIMEOUT                                 5 /* FD timeout in seconds */
+//#define LX200_TIMEOUT                               5 /* FD timeout in seconds */
 #define RB_MAX_LEN                                   64
 #define TCS_TIMEOUT                                   1 /* 50ms? */
 #define TCS_COMMAND_BUFFER_LENGTH                    32
 #define TCS_RESPONSE_BUFFER_LENGTH                   32
+#define TCS_JSON_BUFFER_LENGTH                      256
 #define TCS_NOANSWER                                  0
 
 enum TDirection
@@ -81,22 +83,14 @@ class LX200Skywalker : public LX200Telescope
 
         virtual const char *getDefaultName() override;
         virtual bool Handshake() override;
+        virtual void ISGetProperties(const char *dev)override;
         virtual bool ISNewSwitch(const char *dev, const char *name, ISState *states, char *names[], int n) override;
         virtual bool ISNewNumber(const char *dev, const char *name, double values[], char *names[], int n) override;
         virtual bool updateProperties() override;
         virtual bool initProperties() override;
         virtual bool isSlewComplete() override;
-        virtual void ISGetProperties(const char *dev)override;
-
-        // helper functions
-        virtual bool receive(char* buffer, int wait = TCS_TIMEOUT);
-        virtual bool receive(char* buffer, char end, int wait = TCS_TIMEOUT);
-        virtual void flush();
-        virtual bool transmit(const char* buffer);
-        //virtual bool SetTrackMode(uint8_t mode) override;
 
     protected:
-
         // parking position
         ISwitchVectorProperty MountSetParkSP;
         ISwitch MountSetParkS[1];
@@ -115,81 +109,101 @@ class LX200Skywalker : public LX200Telescope
 
         int controller_format { LX200_LONG_FORMAT };
 
-        // override
-        virtual void getBasicData() override;
-        virtual bool saveConfigItems(FILE *fp) override;
-        virtual bool Goto(double ra, double dec) override;
         virtual bool Connect() override;
         virtual bool Disconnect() override;
-
-        // override INDI::Telescope
-        bool Park() override;
-        bool UnPark() override;
-        bool SetCurrentPark() override;
-        bool SetDefaultPark() override;
-
-        //Skywalker stuff
-        bool SyncDefaultPark();
-        bool SavePark();
-        bool getSystemSlewSpeed (int *xx);
-        bool setSystemSlewSpeed (int xx);
-        bool getJSONData_Y(int jindex, char *jstr, int jstrlen);
-        bool getJSONData_gp(int jindex, char *jstr, int jstrlen);
-        bool notifyPierSide();
+        virtual void getBasicData() override;
+        virtual bool updateLocation(double latitude, double longitude, double elevation) override;
+        virtual bool saveConfigItems(FILE *fp) override;
+        bool checkLX200EquatorialFormat();
+        void notifyPierSide(bool west);
         void notifyMountLock(bool locked);
         void notifyTrackState(INDI::Telescope::TelescopeStatus state);
-        bool MountLocked();
-        bool SetMountLock(bool enable);
-
-
-        // location
-        virtual bool updateLocation(double latitude, double longitude, double elevation) override;
-        bool getTrackFrequency(double *value);
-
-
-        // queries to the scope interface. Wait for specified end character
-        // Unfortunately wait is only defined in seconds in tty_read -> tty_timout
-        virtual bool sendQuery(const char* cmd, char* response, char end, int wait = TCS_TIMEOUT);
-        // Wait for default "#' character
-        virtual bool sendQuery(const char* cmd, char* response, int wait = TCS_TIMEOUT);
-        virtual bool getFirmwareInfo(char* vstring);
-        virtual bool setSiteLatitude(double Lat);
+        virtual bool setLocalDate(uint8_t days, uint8_t months, uint16_t years) override;
+        virtual bool setLocalTime24(uint8_t hour, uint8_t minute, uint8_t second) override;
+        virtual bool setUTCOffset(double offset) override;
         virtual bool setSiteLongitude(double Long);
-
-        bool checkLX200EquatorialFormat();
-        // Guide Commands
+        virtual bool setSiteLatitude(double Lat);
+        bool SetMountLock(bool enable);
+        bool SetCurrentPark() override;
+        bool SetDefaultPark() override;
+        bool getSystemSlewSpeed (int *xx);
+        bool setSystemSlewSpeed (int xx);
+        virtual bool SetSlewRate(int index) override;
+        bool setSlewMode(int slewMode);
+        bool setObjectCoords(double ra, double dec);
+        bool MountTracking();
+        virtual bool SetTrackEnabled(bool enabled) override;
+        virtual bool SetTrackRate(double raRate, double deRate) override;
+        bool getTrackFrequency(double *value);
+        virtual bool Goto(double ra, double dec) override;
+        virtual bool MoveNS(INDI_DIR_NS dir, TelescopeMotionCommand command) override;
+        virtual bool MoveWE(INDI_DIR_WE dir, TelescopeMotionCommand command) override;
+        virtual bool Sync(double ra, double dec) override;
+        bool Park() override;
+        bool UnPark() override;
+        bool SavePark();
+        bool SyncDefaultPark();
+        virtual bool Abort() override;
         virtual IPState GuideNorth(uint32_t ms) override;
         virtual IPState GuideSouth(uint32_t ms) override;
         virtual IPState GuideEast(uint32_t ms) override;
         virtual IPState GuideWest(uint32_t ms) override;
-        virtual bool SetSlewRate(int index) override;
         virtual int SendPulseCmd(int8_t direction, uint32_t duration_msec) override;
-        bool MountTracking();
-        virtual bool SetTrackEnabled(bool enabled) override;
-        virtual bool SetTrackRate(double raRate, double deRate) override;
-        // NSWE Motion Commands
-        virtual bool MoveNS(INDI_DIR_NS dir, TelescopeMotionCommand command) override;
-        virtual bool MoveWE(INDI_DIR_WE dir, TelescopeMotionCommand command) override;
-        virtual bool Sync(double ra, double dec) override;
-        bool setObjectCoords(double ra, double dec);
-        virtual bool setLocalDate(uint8_t days, uint8_t months, uint16_t years) override;
-        virtual bool setLocalTime24(uint8_t hour, uint8_t minute, uint8_t second) override;
-        virtual bool setUTCOffset(double offset) override;
-        //virtual bool getLocalTime(char *timeString) override;
-        //virtual bool getLocalDate(char *dateString) override;
-        //virtual bool getUTFOffset(double *offset) override;
+        //virtual bool SetTrackMode(uint8_t mode) override;
 
-        // Abort ALL motion
-        virtual bool Abort() override;
-        int MoveTo(int direction);
+    private:
+        // helper functions
+        enum class val : uint8_t // scoped enumeration of all JSON-parametervalues "val" (answer to ':gp')
+                {
+                    maxspeed,   // max slewspeed (encoded)
+                    Rlp,        // motorforce x (for EQ: rightascension)
+                    rb3,        // stiffness x
+                    Dlp,        // motorforce y (for EQ: declination)
+                    db3,        // stiffness y
+                    IP1,        // IP address
+                    IP2,
+                    IP3,
+                    IP4,
+                    mask,       // IP mask
+                    port,       // IP port
+                    version,    // FW versionidentifier (EQ: equatorial, AA: horizontal, ...)
+                    lock,       // motorlock
+                    count
+                };
 
-        bool setSlewMode(int slewMode);
+                enum class V1 : uint8_t // scoped enumeration of - so far - required JSON-parametervalues "V1" (answer to ':Y#')
+                {
+                    posX,       // motorposition x
+                    posY,       // motorposition y
+                    stime,      // startime
+                    ubatt,      // voltage (encoded)
+                    db2,        // motorparameter
+                    gstate,     // devicestatus (PierSide encoded)
+                    bstate,     // busstatus
+                    count
+                };
 
+                // std::map<std::string, std::string> JSONtopic{ {":gp", "\"val\""}, {":Y#", "\"V1\""} };
+
+        bool getFirmwareInfo(char* vstring);
+        bool MountLocked();
+        bool PierSideWest();
+        bool getJSONData(const char* cmd, const uint8_t tok_index, char *data);
+        // queries to the scope interface. Wait for specified end character
+        // Unfortunately wait is only defined in seconds in tty_read -> tty_timout
+        bool sendQuery(const char* cmd, char* response, char end, int wait = TCS_TIMEOUT);
+        // Wait for default "#' character
+        bool sendQuery(const char* cmd, char* response, int wait = TCS_TIMEOUT);
+        bool transmit(const char* buffer);
+        bool receive(char* buffer, int wait = TCS_TIMEOUT);
+        bool receive(char* buffer, char end, int wait = TCS_TIMEOUT);
 };
+
 inline bool LX200Skywalker::sendQuery(const char* cmd, char* response, int wait)
 {
     return sendQuery(cmd, response, '#', wait);
 }
+
 inline bool LX200Skywalker::receive(char* buffer, int wait)
 {
     return receive(buffer, '#', wait);
