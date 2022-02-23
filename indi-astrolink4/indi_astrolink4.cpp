1f994c37d65910399ce50208cd557d22569b2f46
Bertrand Lemasle <blemasle@gmail.com>
2022-02-23 09:23:10 +0100

    Astrolink4 : Add 'Auto PWM default ON' settings (#549)

--- indi-astrolink4/indi_astrolink4.cpp
+++ indi-astrolink4/indi_astrolink4.cpp
@@ -16,11 +16,9 @@
  Boston, MA 02110-1301, USA.
 *******************************************************************************/
 #include "indi_astrolink4.h"
+#include "config.h"
 
 #include "indicom.h"
-
-#define VERSION_MAJOR 0
-#define VERSION_MINOR 6
 
 #define ASTROLINK4_LEN      100
 #define ASTROLINK4_TIMEOUT  3
@@ -35,7 +33,7 @@ static std::unique_ptr<IndiAstrolink4> indiAstrolink4(new IndiAstrolink4());
 //////////////////////////////////////////////////////////////////////
 IndiAstrolink4::IndiAstrolink4() : FI(this), WI(this)
 {
-    setVersion(VERSION_MAJOR, VERSION_MINOR);
+    setVersion(ASTROLINK4_VERSION_MAJOR, ASTROLINK4_VERSION_MINOR);
 }
 
 const char * IndiAstrolink4::getDefaultName()
@@ -197,8 +195,19 @@ bool IndiAstrolink4::initProperties()
     IUFillNumberVector(&PWMNP, PWMN, 2, getDeviceName(), "PWM", "PWM", POWER_TAB, IP_RW, 60, IPS_IDLE);
 
     // Auto pwm
-    IUFillSwitch(&AutoPWMS[0], "PWMA_A", "A", ISS_OFF);
-    IUFillSwitch(&AutoPWMS[1], "PWMA_B", "B", ISS_OFF);
+    IUFillSwitch(&AutoPWMDefaultOnS[0], "PWMA_A_DEF_ON", "A", ISS_OFF);
+    IUFillSwitch(&AutoPWMDefaultOnS[1], "PWMA_B_DEF_ON", "B", ISS_OFF);
+    IUFillSwitchVector(&AutoPWMDefaultOnSP, AutoPWMDefaultOnS, 2, getDeviceName(), "AUTO_PWM_DEF_ON", "Auto PWM default ON", SETTINGS_TAB,
+                       IP_RW, ISR_NOFMANY, 60, IPS_IDLE);
+
+    ISState pwmAutoA = ISS_OFF;
+    IUGetConfigSwitch(getDeviceName(), AutoPWMDefaultOnSP.name, AutoPWMDefaultOnS[0].name, &pwmAutoA);
+
+    ISState pwmAutoB = ISS_OFF;
+    IUGetConfigSwitch(getDeviceName(), AutoPWMDefaultOnSP.name, AutoPWMDefaultOnS[1].name, &pwmAutoB);
+
+    IUFillSwitch(&AutoPWMS[0], "PWMA_A", "A", pwmAutoA);
+    IUFillSwitch(&AutoPWMS[1], "PWMA_B", "B", pwmAutoB);
     IUFillSwitchVector(&AutoPWMSP, AutoPWMS, 2, getDeviceName(), "AUTO_PWM", "Auto PWM", POWER_TAB, IP_RW, ISR_NOFMANY, 60,
                        IPS_OK);
 
@@ -271,6 +280,7 @@ bool IndiAstrolink4::updateProperties()
         defineProperty(&CompensationValueNP);
         defineProperty(&CompensateNowSP);
         defineProperty(&PowerDefaultOnSP);
+        defineProperty(&AutoPWMDefaultOnSP);
         defineProperty(&OtherSettingsNP);
         defineProperty(&DCFocDirSP);
         defineProperty(&DCFocTimeNP);
@@ -292,6 +302,7 @@ bool IndiAstrolink4::updateProperties()
         deleteProperty(CompensateNowSP.name);
         deleteProperty(CompensationValueNP.name);
         deleteProperty(PowerDefaultOnSP.name);
+        deleteProperty(AutoPWMDefaultOnSP.name);
         deleteProperty(OtherSettingsNP.name);
         deleteProperty(DCFocTimeNP.name);
         deleteProperty(DCFocDirSP.name);
@@ -535,6 +546,16 @@ bool IndiAstrolink4::ISNewSwitch (const char *dev, const char *name, ISState *st
             return true;
         }
 
+        // Auto PWM default on
+        if (!strcmp(name, AutoPWMDefaultOnSP.name))
+        {
+            IUUpdateSwitch(&AutoPWMDefaultOnSP, states, names, n);
+            AutoPWMDefaultOnSP.s = IPS_OK;
+            saveConfig();
+            IDSetSwitch(&AutoPWMDefaultOnSP, nullptr);
+            return true;
+        }
+
         // Buzzer
         if(!strcmp(name, BuzzerSP.name))
         {
@@ -633,6 +654,7 @@ bool IndiAstrolink4::saveConfigItems(FILE *fp)
     IUSaveConfigNumber(fp, &DCFocTimeNP);
     IUSaveConfigSwitch(fp, &DCFocDirSP);
     IUSaveConfigText(fp, &PowerControlsLabelsTP);
+    IUSaveConfigSwitch(fp, &AutoPWMDefaultOnSP);
     return true;
 }
 
