6fefe32dc74850fdec90dc19fb40d9ae464eb793
RBFocus <75340648+rbfocus@users.noreply.github.com>
2022-10-30 17:36:24 +0100

    Add Driver
    Adding driver for RB Focus Excalibur

---
+++ indi-Excalibur/config.h.cmake
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Define INDI Data Dir */
+#cmakedefine INDI_DATA_DIR "@INDI_DATA_DIR@"
+/* Define Driver version */
+#define EXCALIBUR_VERSION_MAJOR @EXCALIBUR_VERSION_MAJOR@
+#define EXCALIBUR_VERSION_MINOR @EXCALIBUR__VERSION_MINOR@
+
+#endif // CONFIG_H
