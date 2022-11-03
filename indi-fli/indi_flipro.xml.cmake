7f47a0b1a3f0ab712e0bbf356f6dea242694ba10
Jasem Mutlaq <mutlaqja@ikarustech.com>
2022-11-03 09:52:19 +0300

    Add FLIPRO cmake module and adjust driver accordingly

---
+++ indi-fli/indi_flipro.xml.cmake
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="CCDs">
+        <device label="FLI Kepler" manufacturer="Finger Lakes Instruments">
+                <driver name="FLI Kepler">indi_kepler_ccd</driver>
+                <version>@FLI_CCD_VERSION_MAJOR@.@FLI_CCD_VERSION_MINOR@</version>
+        </device>
+</devGroup>
+</driversList>
