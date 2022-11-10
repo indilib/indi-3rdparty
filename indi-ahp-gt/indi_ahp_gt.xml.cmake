030ea308f8204a1754cd079a2c198feb76818ede
Ilia Platone <info@iliaplatone.com>
2022-11-10 18:24:29 +0100

    Add libahp-gt and indi-ahp-gt

---
+++ indi-ahp-gt/indi_ahp_gt.xml.cmake
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="Telescopes">
+        <device label="GT Telescope" manufacturer="AHP">
+                <driver name="GT Telescope">indi_ahp_gt</driver>
+                <version>@AHP_GT_VERSION_MAJOR@.@AHP_GT_VERSION_MINOR@</version>
+	</device>
+</devGroup>
+</driversList>
+
