2e41da8b4edd21d3077ac16621d5c17b3c5523aa
Frank Chen <frank.chen@astroasis.com>
2023-05-07 15:33:21 +0800

    Initial version for Oasis Focuser INDI driver

---
+++ indi-astroasis/config.h.cmake
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Define INDI Data Dir */
+#cmakedefine INDI_DATA_DIR "@INDI_DATA_DIR@"
+/* Define Driver version */
+#define ASTROASIS_VERSION_MAJOR @ASTROASIS_VERSION_MAJOR@
+#define ASTROASIS_VERSION_MINOR @ASTROASIS_VERSION_MINOR@
+
+#endif // CONFIG_H
