38f0b5389cd447311223b11d7431af33c215e959
Ilia Platone <info@iliaplatone.com>
2023-06-15 16:18:13 +0200

    Missing character in CMakeLists.txt

---
+++ libahp-xc/build/indi-3rdparty-libs/libapogee/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Define Configuration Directory */
+#define APOGEE_CONF_DIR "/etc"
+
+
+#endif // CONFIG_H
---
+++ libahp-xc/build/indi-3rdparty-libs/libqsi/config.h
@@ -0,0 +1,8 @@
+/* Package version */
+#define PACKAGE_VERSION 7.6.0
+
+/* Using FTDI */
+/* #undef USELIBFTDIZERO */
+
+
+
