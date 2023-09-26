87a7e3d7212981d523ddc54e50d8cfb60caff91b
astroasis <132460368+astroasis@users.noreply.github.com>
2023-09-26 20:53:44 +0800

    Fix spelling (#841)

--- indi-astroasis/indi_astroasis.xml.cmake
+++ indi-astroasis/indi_astroasis.xml.cmake
@@ -2,7 +2,7 @@
 <driversList>
 <devGroup group="Focusers">
         <device label="Oasis Focuser" manufacturer="Astroasis">
-                <driver name="Oasis Focusr">indi_oasis_focuser</driver>
+                <driver name="Oasis Focuser">indi_oasis_focuser</driver>
                 <version>@ASTROASIS_VERSION_MAJOR@.@ASTROASIS_VERSION_MINOR@</version>
         </device>
 </devGroup>
