7b86b69e3bd206c41f0a17b043510ce6a7199dba
mcgillca <colin.a.mcgill@gmail.com>
2024-08-19 12:01:55 +0100

    Initial Version of AStarBox
    Initial version of AStarBox as a 3rd-party driver.

---
+++ indi-astarbox/indi_astarbox.xml.cmake
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="Auxiliary">
+	<device label="AStarBox">
+		<driver name="AStarBox">indi_astarbox</driver>
+		<version>@VERSION_MAJOR@.@VERSION_MINOR@</version>
+	</device>
+</devGroup>
+</driversList>
