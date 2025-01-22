17c305f43162edcef18e83f32ccc457dca21954a
JDK79 <jdk.ssvw+github@gmail.com>
2025-01-22 07:57:48 +0100

    QHY Focuser Optional Board Version (#1027)
    * Update qhy_focuser.cpp
    
    make cmd_version_board optional
    
    * Update qhy_focuser.cpp

--- indi-qhy/qhy_focuser.cpp
+++ indi-qhy/qhy_focuser.cpp
@@ -271,12 +271,18 @@ int QFocuser::ReadResponse(char *buf, int &cmd_id)
                 else if (cmd_id == 1)
                 {
                     LOGF_DEBUG("<RES> %s", cmd_json.dump().c_str());
-                    if (cmd_json.find("id") != cmd_json.end() && cmd_json.find("version") != cmd_json.end() && cmd_json.find("bv") != cmd_json.end())
+                    
+                    if (cmd_json.find("id") == cmd_json.end()) { return -1; }
+		    if (cmd_json.find("version") == cmd_json.end()) { return -1; }
+                    
+		    cmd_version = cmd_json["version"];                 
+
+		    if (cmd_json.find("bv") != cmd_json.end())
                     {
-                        cmd_version = cmd_json["version"];
                         cmd_version_board = cmd_json["bv"];
-                        return bytesRead;  // 返回字节数
                     }
+
+                    return bytesRead;  // 返回字节数
                 }
                 else if (cmd_id == 4)
                 {
