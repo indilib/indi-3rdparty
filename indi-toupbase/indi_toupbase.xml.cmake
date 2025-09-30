952cc2167746cb85c5652c8754318c9ab08a60f3
Lehel Gyuro <lehel@freemail.hu>
2025-09-30 22:01:40 +0200

    Reduce code duplication of touptek builder
    Reduce the code duplication by the use of foreach loops and macros in
    touptek builders.

--- indi-toupbase/indi_toupbase.xml.cmake
+++ indi-toupbase/indi_toupbase.xml.cmake
@@ -1,20 +1,20 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <driversList>
   <devGroup group="CCDs">
-    <device label="@MAKER@" mdpd="true" manufacturer="@MAKER@">
-      <driver name="@DRIVER@">indi_@DRIVER_NAME@_ccd</driver>
+    <device label="@LABEL@" mdpd="true" manufacturer="@MANUFACTURER@">
+      <driver name="@DRIVER_NAME@">indi_@DRIVER_LIB_NAME@_ccd</driver>
       <version>@TOUPBASE_VERSION_MAJOR@.@TOUPBASE_VERSION_MINOR@</version>
     </device>
   </devGroup>
   <devGroup group="Filter Wheels">
-    <device label="@MAKER@" mdpd="true" manufacturer="@MAKER@">
-      <driver name="@DRIVER@">indi_@DRIVER_NAME@_wheel</driver>
+    <device label="@LABEL@" mdpd="true" manufacturer="@MANUFACTURER@">
+      <driver name="@DRIVER_NAME@">indi_@DRIVER_LIB_NAME@_wheel</driver>
       <version>@TOUPBASE_VERSION_MAJOR@.@TOUPBASE_VERSION_MINOR@</version>
     </device>
   </devGroup>
   <devGroup group="Focusers">
-    <device label="@MAKER@" mdpd="true" manufacturer="@MAKER@">
-      <driver name="@DRIVER@">indi_@DRIVER_NAME@_focuser</driver>
+    <device label="@LABEL@" mdpd="true" manufacturer="@MANUFACTURER@">
+      <driver name="@DRIVER_NAME@">indi_@DRIVER_LIB_NAME@_focuser</driver>
       <version>@TOUPBASE_VERSION_MAJOR@.@TOUPBASE_VERSION_MINOR@</version>
     </device>
   </devGroup>
