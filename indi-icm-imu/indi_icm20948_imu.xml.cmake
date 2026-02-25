dbcb8264eb4b61c83e2a2676f0f6d391bbf163fb
Jasem Mutlaq <mutlaqja@ikarustech.com>
2026-02-25 11:23:17 +0300

    Initial support for ICM 20948 IMU driver

---
+++ indi-icm-imu/indi_icm20948_imu.xml.cmake
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+    <devGroup group="Detectors">
+        <device label="ICM-20948 IMU" manufacturer="TDK InvenSense">
+            <driver name="ICM-20948 IMU">indi_icm20948_imu</driver>
+            <version>@ICM_VERSION_MAJOR@.@ICM_VERSION_MINOR@</version>
+        </device>
+    </devGroup>
+</driversList>
