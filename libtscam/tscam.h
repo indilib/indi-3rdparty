190c16b024c4cd61c8331e862f081a6f61c518fb
ToupTek Photonics Co., Ltd. <41673153+touptek@users.noreply.github.com>
2026-03-23 12:12:05 +0800

    update Touptek & oem lib, version: 59.31026.20260322 (#1291)

--- libtscam/tscam.h
+++ libtscam/tscam.h
@@ -1,7 +1,7 @@
 ﻿#ifndef __tscam_h__
 #define __tscam_h__
 
-/* Version: 59.30594.20260120 */
+/* Version: 59.31026.20260322 */
 /*
    Platform & Architecture:
        (1) Win32:
@@ -112,7 +112,7 @@ extern "C" {
 #define E_UNEXPECTED        (HRESULT)(0x8000ffff) /* Catastrophic failure */ /* Remark: Generally indicates that the conditions are not met, such as calling put_Option setting some options that do not support modification when the camera is running, and so on */
 #define E_NOTIMPL           (HRESULT)(0x80004001) /* Not supported or not implemented */ /* Remark: This feature is not supported on this model of camera */
 #define E_NOINTERFACE       (HRESULT)(0x80004002)
-#define E_ACCESSDENIED      (HRESULT)(0x80070005) /* Permission denied */ /* Remark: The program on Linux does not have permission to open the USB device, please enable udev rules file or run as root */
+#define E_ACCESSDENIED      (HRESULT)(0x80070005) /* Permission denied */ /* Remark: Insufficient permissions. This may be blocked by system security policies; on Linux, USB devices often require additional permission configuration, which can be resolved by setting up udev rules or running with root privileges */
 #define E_OUTOFMEMORY       (HRESULT)(0x8007000e) /* Out of memory */
 #define E_INVALIDARG        (HRESULT)(0x80070057) /* One or more arguments are not valid */
 #define E_POINTER           (HRESULT)(0x80004003) /* Pointer that is not valid */ /* Remark: Pointer is NULL */
@@ -278,10 +278,10 @@ typedef struct Tscam_t { int unused; } *HTscam;
 #define TSCAM_ANTIBLOOMING_MIN          0       /* Anti Blooming */
 #define TSCAM_GVCP_RETRY_DEF            4       /* GVCP Retry */
 #define TSCAM_GVCP_RETRY_MIN            2
-#define TSCAM_GVCP_RETRY_MAX            16
-#define TSCAM_GVCP_TIMEOUT_DEF          15      /* GVCP Timeout */
-#define TSCAM_GVCP_TIMEOUT_MIN          5
-#define TSCAM_GVCP_TIMEOUT_MAX          150
+#define TSCAM_GVCP_RETRY_MAX            20
+#define TSCAM_GVCP_TIMEOUT_DEF          40      /* GVCP Timeout */
+#define TSCAM_GVCP_TIMEOUT_MIN          20
+#define TSCAM_GVCP_TIMEOUT_MAX          200
 #define TSCAM_GVSP_WAIT_PERCENT_DEF     1       /* GVSP wait percent */
 #define TSCAM_GVSP_WAIT_PERCENT_MIN     0
 #define TSCAM_GVSP_WAIT_PERCENT_MAX     100
@@ -329,7 +329,7 @@ typedef struct {
 } TscamDeviceV2; /* device instance for enumerating */
 
 /*
-    get the version of this dll/so/dylib, which is: 59.30594.20260120
+    get the version of this dll/so/dylib, which is: 59.31026.20260322
 */
 #if defined(_WIN32)
 TSCAM_API(const wchar_t*)   Tscam_Version();
@@ -352,10 +352,10 @@ TSCAM_API(unsigned) Tscam_EnumV2(TscamDeviceV2 arr[TSCAM_MAX]);
 
 /* use the camId of TscamDeviceV2, which is enumerated by Tscam_EnumV2.
     if camId is NULL, Tscam_Open will open the first enumerated camera.
-    For USB, GigE, CameraLink or CXP camera, the camId can also be specified as (case sensitive):
+    For USB, GigE, CameraLink or CXP camera, the camId can also be specified as (case sensitive, no spaces):
         (a) "sn:xxxxxxxxxxxx" (Use SN, such as sn:ZP250212241204105), or
         (b) "name:xxxxxx" (Use user-defined name, such as name:Camera1)
-    Moreover, for GigE camera, the camId can also be specified as (case sensitive):
+    Moreover, for GigE camera, the camId can also be specified as (case sensitive, no spaces):
         (a) "ip:xxx.xxx.xxx.xxx" (Use IP address, such as ip:192.168.1.100), or
         (b) "mac:xxxxxxxxxxxx" (Use MAC address, such as mac:d05f64ffff23)
     For the issue of opening the camera on Android, please refer to the documentation
@@ -1462,6 +1462,9 @@ TSCAM_API(HRESULT)  Tscam_CtiEnable(PTSCAM_HOTPLUG funHotPlug, void* ctxHotPlug,
 #else
 TSCAM_API(HRESULT)  Tscam_CtiEnable(PTSCAM_HOTPLUG funHotPlug, void* ctxHotPlug, const char* ctiPath[]);
 #endif
+
+TSCAM_API(HRESULT) Tscam_readPtr(HTscam h, const char* key, int len, void* pData);
+TSCAM_API(HRESULT) Tscam_writePtr(HTscam h, const char* key, int len, const void* pData);
 
 /*
  filePath:
